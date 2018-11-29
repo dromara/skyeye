5384021424e2bd1492d0fd1c9980e33f2ae34041
weizhiqiang <598748873@qq.com>
2018-11-29 09:58:37 +0800

    公司部门编辑，删除完成

--- skyeye-web/src/main/webapp/js/companydepartment/companydepartmentedit.js
+++ skyeye-web/src/main/webapp/js/companydepartment/companydepartmentedit.js
@@ -3,14 +3,15 @@ layui.config({
 	version: skyeyeVersion
 }).define(['jquery', 'winui'], function (exports) {
 	winui.renderColor();
-	layui.use(['form'], function (form) {
+	layui.use(['form', 'layedit'], function (form) {
 		var index = parent.layer.getFrameIndex(window.name); //获取窗口索引
 	    var $ = layui.$,
-	    form = layui.form;
+	    form = layui.form,
+	    layedit = layui.layedit;
 	    
 		showGrid({
 		 	id: "showForm",
-		 	url: reqBasePath   "icon004",
+		 	url: reqBasePath + "companydepartment004",
 		 	params: {rowId: parent.rowId},
 		 	pagination: false,
 		 	template: getFileContent('tpl/companydepartment/companydepartmenteditTemplate.tpl'),
@@ -18,16 +19,35 @@ layui.config({
 		 	},
 		 	ajaxSendAfter:function(json){
 		 		
+		 		layContent = layedit.build('content', {
+			    	tool: [
+		    	       'strong' //加粗
+		    	       ,'italic' //斜体
+		    	       ,'underline' //下划线
+		    	       ,'del' //删除线
+		    	       ,'|' //分割线
+		    	       ,'left' //左对齐
+		    	       ,'center' //居中对齐
+		    	       ,'right' //右对齐
+		    	       ,'link' //超链接
+		    	       ,'unlink' //清除链接
+		    	       ,'face' //表情
+		    	     ]
+			    });
+		 		
 		 		form.render();
 		 		
 		 		form.on('submit(formEditBean)', function (data) {
 			    	//表单验证
 			        if (winui.verifyForm(data.elem)) {
 			        	var params = {
-		        			
+		        			departmentName: $("#departmentName").val(),
+		        			departmentDesc: encodeURI(layedit.getContent(layContent)),
+		        			companyId: parent.companyId,
+		        			rowId: parent.rowId,
 			        	};
 			        	
-			        	AjaxPostUtil.request({url:reqBasePath   "icon005", params:params, type:'json', callback:function(json){
+			        	AjaxPostUtil.request({url:reqBasePath + "companydepartment005", params:params, type:'json', callback:function(json){
 			 	   			if(json.returnCode == 0){
 				 	   			parent.layer.close(index);
 				 	        	parent.refreshCode = '0';
--- skyeye-web/src/main/webapp/tpl/companydepartment/companydepartmenteditTemplate.tpl
+++ skyeye-web/src/main/webapp/tpl/companydepartment/companydepartmenteditTemplate.tpl
@@ -1,5 +1,16 @@
 {{#bean}}
-    
+    <div class="layui-form-item">
+        <label class="layui-form-label">部门名称<i class="red">*</i></label>
+        <div class="layui-input-block">
+        	<input type="text" id="departmentName" name="departmentName" win-verify="required" placeholder="请输入部门名称" class="layui-input" maxlength="50" value="{{departmentName}}"/>
+        </div>
+    </div>
+    <div class="layui-form-item">
+        <label class="layui-form-label">部门简介</label>
+        <div class="layui-input-block">
+        	<textarea id="content" name="content" style="display: none;">{{departmentDesc}}</textarea>
+        </div>
+    </div>
     <div class="layui-form-item">
         <div class="layui-input-block">
             <button class="winui-btn" id="cancle">取消</button>
