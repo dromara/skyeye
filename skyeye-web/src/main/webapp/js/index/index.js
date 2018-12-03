be9d8e4bd8f5b0d2a9045add5748956b9c909618
weizhiqiang <598748873@qq.com>
2018-12-03 17:35:26 +0800

    添加通讯好友

--- skyeye-web/src/main/webapp/js/index/index.js
+++ skyeye-web/src/main/webapp/js/index/index.js
@@ -6,8 +6,9 @@
     desktop: 'js/winui.desktop',//桌面加载模块
     start: 'js/winui.start',//左下角开始菜单
     helper: 'js/winui.helper'
-}).define(['window', 'desktop', 'start', 'helper'], function (exports) {
-    var $ = layui.jquery;
+}).define(['window', 'desktop', 'start', 'helper', 'layim'], function (exports) {
+    var $ = layui.jquery,
+    layim = layui.layim;
     
     $(function () {
     	
@@ -42,12 +43,174 @@
    					json.bean.winLockBgPicUrl = fileBasePath + json.bean.winLockBgPicUrl;
    				}
    		        initWinConfig(json);
+   		        
+   		        //加载聊天
+   		        initTalk();
+   		        
    			}else{
    				location.href = "login.html";
    			}
    		}});
     	
     });
+    
+    //自动回复
+    var autoReplay = [
+		'您好，我现在有事不在，一会再和您联系。',
+		'你没发错吧？face[微笑] ',
+		'洗澡中，请勿打扰，偷窥请购票，个体四十，团体八折，订票电话：一般人我不告诉他！face[哈哈] ',
+		'你好，我是主人的美女秘书，有什么事就跟我说吧，等他回来我会转告他的。face[心] face[心] face[心] ',
+		'face[威武] face[威武] face[威武] face[威武] ',
+		'<（@￣︶￣@）>',
+		'你要和我说话？你真的要和我说话？你确定自己想说吗？你一定非说不可吗？那你说吧，这是自动回复。',
+		'face[黑线]  你慢慢说，别急……',
+		'(*^__^*) face[嘻嘻] ，是天眼吗？'
+	];
+    
+    function initTalk(){
+    	layim.config({
+			brief: false,// 是否简约模式（如果true则不显示主面板）
+			init: {
+				url: 'getList.json',
+				data: {}
+			},//好友接口
+			members: {
+				url: 'getMembers.json',
+				data: {}
+			},//群员借口
+			uploadImage: {
+				url: '/upload/image', //（返回的数据格式见下文）
+				type: '' //默认post
+			},//上传图片接口
+			uploadFile: {
+				url: '/upload/file', //（返回的数据格式见下文）
+				type: '' //默认post
+			},//上传文件接口
+			tool: [{
+				alias: 'code',
+				title: '代码',
+				icon: '&#xe64e;'
+			}],//扩展工具栏
+			initSkin: '5.jpg', //1-5 设置初始背景
+			notice: true, //是否开启桌面消息提醒，默认false
+			msgbox: layui.cache.dir + 'css/modules/layim/html/msgbox.html', //消息盒子页面地址，若不开启，剔除该项即可
+			find: layui.cache.dir + 'css/modules/layim/html/find.html', //发现页面地址，若不开启，剔除该项即可
+			chatLog: layui.cache.dir + 'css/modules/layim/html/chatLog.html', //聊天记录页面地址，若不开启，剔除该项即可
+		});
+    	
+    	//监听在线状态的切换事件
+		layim.on('online', function(data) {
+			//console.log(data);
+		});
+		
+		//监听签名修改
+		layim.on('sign', function(value) {
+			//console.log(value);
+		});
+		
+		layim.on('tool(code)', function(insert) {
+			layer.prompt({
+				title: '插入代码',
+				formType: 2,
+				shade: 0
+			}, function(text, index) {
+				layer.close(index);
+				insert('[pre class=layui-code]' + text + '[/pre]'); //将内容插入到编辑器
+			});
+		});
+		
+		//监听layim建立就绪
+		layim.on('ready', function(res) {
+
+			//console.log(res.mine);
+			layim.msgbox(5); //模拟消息盒子有新消息，实际使用时，一般是动态获得
+
+			//添加好友（如果检测到该socket）
+			layim.addList({
+				type: 'group',
+				avatar: "http://tva3.sinaimg.cn/crop.64.106.361.361.50/7181dbb3jw8evfbtem8edj20ci0dpq3a.jpg",
+				groupname: 'Angular开发',
+				id: "12333333",
+				members: 0
+			});
+			layim.addList({
+				type: 'friend',
+				avatar: "http://tp2.sinaimg.cn/2386568184/180/40050524279/0",
+				username: '冲田杏梨',
+				groupid: 2,
+				id: "1233333312121212",
+				remark: "本人冲田杏梨将结束AV女优的工作"
+			});
+
+			setTimeout(function() {
+				//接受消息（如果检测到该socket）
+//				layim.getMessage({
+//					username: "Hi",
+//					avatar: "http://qzapp.qlogo.cn/qzapp/100280987/56ADC83E78CEC046F8DF2C5D0DD63CDE/100",
+//					id: "10000111",
+//					type: "friend",
+//					content: "临时：" + new Date().getTime()
+//				});
+
+			}, 3000);
+		});
+		
+		//监听发送消息
+		layim.on('sendMessage', function(data) {
+			var To = data.to;
+			//console.log(data);
+			if(To.type === 'friend') {
+				layim.setChatStatus('<span style="color:#FF5722;">对方正在输入。。。</span>');
+			}
+
+			//演示自动回复
+			setTimeout(function() {
+				var obj = {};
+				if(To.type === 'group') {
+					obj = {
+						username: '模拟群员' + (Math.random() * 100 | 0),
+						avatar: layui.cache.dir + 'images/face/' + (Math.random() * 72 | 0) + '.gif',
+						id: To.id,
+						type: To.type,
+						content: autoReplay[Math.random() * 9 | 0]
+					};
+				} else {
+					obj = {
+						username: To.name,
+						avatar: To.avatar,
+						id: To.id,
+						type: To.type,
+						content: autoReplay[Math.random() * 9 | 0]
+					};
+					layim.setChatStatus('<span style="color:#FF5722;">在线</span>');
+				}
+				layim.getMessage(obj);
+			}, 1000);
+		});
+		
+		//监听查看群员
+		layim.on('members', function(data) {
+			//console.log(data);
+		});
+
+		//监听聊天窗口的切换
+		layim.on('chatChange', function(res) {
+			var type = res.data.type;
+			console.log(res.data.id)
+			if(type === 'friend') {
+				//模拟标注好友状态
+				//layim.setChatStatus('<span style="color:#FF5722;">在线</span>');
+			} else if(type === 'group') {
+				//模拟系统消息
+				layim.getMessage({
+					system: true,
+					id: res.data.id,
+					type: "group",
+					content: '模拟群员' + (Math.random() * 100 | 0) + '加入群聊'
+				});
+			}
+		});
+    }
     
     //初始化配置信息
     function initWinConfig(json){
