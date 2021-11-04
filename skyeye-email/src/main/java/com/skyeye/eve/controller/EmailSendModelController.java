43b5eba9279f52b0604c6389de9c7abb8bb22725
weizhiqiang <598748873@qq.com>
2021-11-04 23:04:37 +0800

    代码格式调整

--- skyeye-email/src/main/java/com/skyeye/eve/controller/EmailSendModelController.java
+++ skyeye-email/src/main/java/com/skyeye/eve/controller/EmailSendModelController.java
@@ -1,6 +1,7 @@
 /*******************************************************************************
  * Copyright 卫志强 QQ：598748873@qq.com Inc. All rights reserved. 开源地址：https://gitee.com/doc_wei01/skyeye
  ******************************************************************************/
+
 package com.skyeye.eve.controller;
 
 import com.skyeye.common.object.InputObject;
