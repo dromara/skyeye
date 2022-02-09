51c159f73d03f83b6aefae09019de417e3562321
weizhiqiang <598748873@qq.com>
2022-02-09 12:48:46 +0800

    门店信息新增地址字段

--- shop/src/main/resources/template/js/store/storeAdd.js
+++ shop/src/main/resources/template/js/store/storeAdd.js
@@ -21,13 +21,34 @@ layui.config({
             $("#areaId").html(getDataUseHandlebars(getFileContent('tpl/template/select-option.tpl'), json));
         });
 
+        // 加载行政区划-省
+        loadChildProvinceArea();
+
 	    matchingLanguage();
         form.render();
         form.on('submit(formAddBean)', function (data) {
             if (winui.verifyForm(data.elem)) {
+                var provinceId = "", cityId = "", addressAreaId = "", townshipId = "";
+                if(!isNull($("#provinceId").val())){
+                    provinceId = $("#provinceId").val();
+                }
+                if(!isNull($("#cityId").val())){
+                    cityId = $("#cityId").val();
+                }
+                if(!isNull($("#addressAreaId").val())){
+                    addressAreaId = $("#addressAreaId").val();
+                }
+                if(!isNull($("#townshipId").val())){
+                    townshipId = $("#townshipId").val();
+                }
                 var params = {
                     name: $("#name").val(),
                     areaId: $("#areaId").val(),
+                    provinceId: provinceId,
+                    cityId: cityId,
+                    addressAreaId: addressAreaId,
+                    townshipId: townshipId,
+                    addressDetailed: $("#addressDetailed").val(),
                     remark: $("#remark").val()
                 };
                 AjaxPostUtil.request({url: shopBasePath + "store002", params: params, type: 'json', method: "POST", callback: function(json){
@@ -41,6 +62,96 @@ layui.config({
             }
             return false;
         });
+
+        form.on('select(areaProvince)', function(data){
+            layui.$(data.elem).parent('dd').nextAll().remove();
+            if(isNull(data.value) || data.value == '请选择'){
+            }else{
+                loadChildCityArea();
+            }
+        });
+        form.on('select(areaCity)', function(data){
+            layui.$(data.elem).parent('dd').nextAll().remove();
+            if(isNull(data.value) || data.value == '请选择'){
+            }else{
+                loadChildArea();
+            }
+        });
+        form.on('select(area)', function(data){
+            layui.$(data.elem).parent('dd').nextAll().remove();
+            if(isNull(data.value) || data.value == '请选择'){
+            }else{
+                loadChildAreaTownShip();
+            }
+        });
+
+        // 省级行政区划
+        function loadChildProvinceArea(){
+            AjaxPostUtil.request({url:reqBasePath + "commontarea001", params:{}, type:'json', method: "POST", callback:function(json){
+                if(json.returnCode == 0){
+                    var str = '<dd class="layui-col-xs3"><select id="provinceId" win-verify="required" lay-filter="areaProvince" lay-search=""><option value="">请选择</option>';
+                    for(var i = 0; i < json.rows.length; i++){
+                        str += '<option value="' + json.rows[i].id + '">' + json.rows[i].name + '</option>';
+                    }
+                    str += '</select></dd>';
+                    $("#lockParentSel").append(str);
+                    form.render('select');
+                }else{
+                    winui.window.msg(json.returnMessage, {icon: 2,time: 2000});
+                }
+            }});
+        }
+
+        // 市级行政区划
+        function loadChildCityArea(){
+            AjaxPostUtil.request({url:reqBasePath + "commontarea002", params:{rowId: $("#provinceId").val()}, type:'json', method: "POST", callback:function(json){
+                if(json.returnCode == 0){
+                    var str = '<dd class="layui-col-xs3"><select id="cityId" lay-filter="areaCity" lay-search=""><option value="">请选择</option>';
+                    for(var i = 0; i < json.rows.length; i++){
+                        str += '<option value="' + json.rows[i].id + '">' + json.rows[i].name + '</option>';
+                    }
+                    str += '</select></dd>';
+                    $("#lockParentSel").append(str);
+                    form.render('select');
+                }else{
+                    winui.window.msg(json.returnMessage, {icon: 2,time: 2000});
+                }
+            }});
+        }
+
+        // 县级行政区划
+        function loadChildArea(){
+            AjaxPostUtil.request({url:reqBasePath + "commontarea003", params:{rowId: $("#cityId").val()}, type:'json', method: "POST", callback:function(json){
+                if(json.returnCode == 0){
+                    var str = '<dd class="layui-col-xs3"><select id="addressAreaId" lay-filter="area" lay-search=""><option value="">请选择</option>';
+                    for(var i = 0; i < json.rows.length; i++){
+                        str += '<option value="' + json.rows[i].id + '">' + json.rows[i].name + '</option>';
+                    }
+                    str += '</select></dd>';
+                    $("#lockParentSel").append(str);
+                    form.render('select');
+                }else{
+                    winui.window.msg(json.returnMessage, {icon: 2,time: 2000});
+                }
+            }});
+        }
+
+        // 镇级行政区划
+        function loadChildAreaTownShip(){
+            AjaxPostUtil.request({url:reqBasePath + "commontarea004", params:{rowId: $("#addressAreaId").val()}, type:'json', method: "POST", callback:function(json){
+                if(json.returnCode == 0){
+                    var str = '<dd class="layui-col-xs3"><select id="townshipId" lay-filter="areaTownShip" lay-search=""><option value="">请选择</option>';
+                    for(var i = 0; i < json.rows.length; i++){
+                        str += '<option value="' + json.rows[i].id + '">' + json.rows[i].name + '</option>';
+                    }
+                    str += '</select></dd>';
+                    $("#lockParentSel").append(str);
+                    form.render('select');
+                }else{
+                    winui.window.msg(json.returnMessage, {icon: 2,time: 2000});
+                }
+            }});
+        }
 
         $("body").on("click", "#cancle", function(){
             parent.layer.close(index);
--- shop/src/main/resources/template/js/store/storeEdit.js
+++ shop/src/main/resources/template/js/store/storeEdit.js
@@ -30,15 +30,36 @@ layui.config({
                     $("#areaId").html(getDataUseHandlebars(getFileContent('tpl/template/select-option.tpl'), json));
                     $("#areaId").val(data.bean.areaId);
                 });
-            	
+
+                // 公司地址
+                initArea(data.bean);//加载省级行政区划
+
 			    matchingLanguage();
                 form.render();
                 form.on('submit(formEditBean)', function (data) {
                     if (winui.verifyForm(data.elem)) {
+                        var provinceId = "", cityId = "", addressAreaId = "", townshipId = "";
+                        if(!isNull($("#provinceId").val())){
+                            provinceId = $("#provinceId").val();
+                        }
+                        if(!isNull($("#cityId").val())){
+                            cityId = $("#cityId").val();
+                        }
+                        if(!isNull($("#addressAreaId").val())){
+                            addressAreaId = $("#addressAreaId").val();
+                        }
+                        if(!isNull($("#townshipId").val())){
+                            townshipId = $("#townshipId").val();
+                        }
                         var params = {
                             rowId: parent.rowId,
                             name: $("#name").val(),
                             areaId: $("#areaId").val(),
+                            provinceId: provinceId,
+                            cityId: cityId,
+                            addressAreaId: addressAreaId,
+                            townshipId: townshipId,
+                            addressDetailed: $("#addressDetailed").val(),
                             remark: $("#remark").val()
                         };
                         AjaxPostUtil.request({url: shopBasePath + "store005", params: params, type: 'json', method: "PUT", callback: function(json){
@@ -54,6 +75,162 @@ layui.config({
                 });
             }
         });
+
+        //初始化行政区划-省
+        function initArea(bean){
+            AjaxPostUtil.request({url:reqBasePath + "commontarea001", params:{}, type:'json', method: "POST", callback:function(json){
+                if(json.returnCode == 0){
+                    var str = '<dd class="layui-col-xs3"><select id="provinceId" win-verify="required" lay-filter="areaProvince" lay-search=""><option value="">请选择</option>';
+                    for(var i = 0; i < json.rows.length; i++){
+                        str += '<option value="' + json.rows[i].id + '">' + json.rows[i].name + '</option>';
+                    }
+                    str += '</select></dd>';
+                    $("#lockParentSel").append(str);
+                    if(!isNull(bean.provinceId)){
+                        $("#provinceId").val(bean.provinceId);
+                        initAreaCity(bean);
+                    }
+                    form.render('select');
+                }else{
+                    winui.window.msg(json.returnMessage, {icon: 2,time: 2000});
+                }
+            }});
+        }
+
+        //初始化行政区划-市
+        function initAreaCity(bean){
+            AjaxPostUtil.request({url:reqBasePath + "commontarea002", params:{rowId: $("#provinceId").val()}, type:'json', method: "POST", callback:function(json){
+                if(json.returnCode == 0){
+                    var str = '<dd class="layui-col-xs3"><select id="cityId" lay-filter="areaCity" lay-search=""><option value="">请选择</option>';
+                    for(var i = 0; i < json.rows.length; i++){
+                        str += '<option value="' + json.rows[i].id + '">' + json.rows[i].name + '</option>';
+                    }
+                    str += '</select></dd>';
+                    $("#lockParentSel").append(str);
+                    if(!isNull(bean.cityId)){
+                        $("#cityId").val(bean.cityId);
+                        initAreaChildArea(bean);
+                    }
+                    form.render('select');
+                }else{
+                    winui.window.msg(json.returnMessage, {icon: 2,time: 2000});
+                }
+            }});
+        }
+
+        //初始化行政区划-县
+        function initAreaChildArea(bean){
+            AjaxPostUtil.request({url:reqBasePath + "commontarea003", params:{rowId: $("#cityId").val()}, type:'json', method: "POST", callback:function(json){
+                if(json.returnCode == 0){
+                    var str = '<dd class="layui-col-xs3"><select id="addressAreaId" lay-filter="area" lay-search=""><option value="">请选择</option>';
+                    for(var i = 0; i < json.rows.length; i++){
+                        str += '<option value="' + json.rows[i].id + '">' + json.rows[i].name + '</option>';
+                    }
+                    str += '</select></dd>';
+                    $("#lockParentSel").append(str);
+                    if(!isNull(bean.addressAreaId)){
+                        $("#addressAreaId").val(bean.addressAreaId);
+                        initAreaTownShip(bean);
+                    }
+                    form.render('select');
+                }else{
+                    winui.window.msg(json.returnMessage, {icon: 2,time: 2000});
+                }
+            }});
+        }
+
+        //初始化行政区划-镇
+        function initAreaTownShip(bean){
+            AjaxPostUtil.request({url:reqBasePath + "commontarea004", params:{rowId: $("#addressAreaId").val()}, type:'json', method: "POST", callback:function(json){
+                if(json.returnCode == 0){
+                    var str = '<dd class="layui-col-xs3"><select id="townshipId" lay-filter="areaTownShip" lay-search=""><option value="">请选择</option>';
+                    for(var i = 0; i < json.rows.length; i++){
+                        str += '<option value="' + json.rows[i].id + '">' + json.rows[i].name + '</option>';
+                    }
+                    str += '</select></dd>';
+                    $("#lockParentSel").append(str);
+                    if(!isNull(bean.townshipId)){
+                        $("#townshipId").val(bean.townshipId);
+                    }
+                    form.render('select');
+                }else{
+                    winui.window.msg(json.returnMessage, {icon: 2,time: 2000});
+                }
+            }});
+        }
+
+        form.on('select(areaProvince)', function(data){
+            layui.$(data.elem).parent('dd').nextAll().remove();
+            if(isNull(data.value) || data.value == '请选择'){
+            }else{
+                loadChildCityArea();
+            }
+        });
+        form.on('select(areaCity)', function(data){
+            layui.$(data.elem).parent('dd').nextAll().remove();
+            if(isNull(data.value) || data.value == '请选择'){
+            }else{
+                loadChildArea();
+            }
+        });
+        form.on('select(area)', function(data){
+            layui.$(data.elem).parent('dd').nextAll().remove();
+            if(isNull(data.value) || data.value == '请选择'){
+            }else{
+                loadChildAreaTownShip();
+            }
+        });
+
+        //市级行政区划
+        function loadChildCityArea(){
+            AjaxPostUtil.request({url:reqBasePath + "commontarea002", params:{rowId: $("#provinceId").val()}, type:'json', method: "POST", callback:function(json){
+                if(json.returnCode == 0){
+                    var str = '<dd class="layui-col-xs3"><select id="cityId" lay-filter="areaCity" lay-search=""><option value="">请选择</option>';
+                    for(var i = 0; i < json.rows.length; i++){
+                        str += '<option value="' + json.rows[i].id + '">' + json.rows[i].name + '</option>';
+                    }
+                    str += '</select></dd>';
+                    $("#lockParentSel").append(str);
+                    form.render('select');
+                }else{
+                    winui.window.msg(json.returnMessage, {icon: 2,time: 2000});
+                }
+            }});
+        }
+
+        //县级行政区划
+        function loadChildArea(){
+            AjaxPostUtil.request({url:reqBasePath + "commontarea003", params:{rowId: $("#cityId").val()}, type:'json', method: "POST", callback:function(json){
+                if(json.returnCode == 0){
+                    var str = '<dd class="layui-col-xs3"><select id="addressAreaId" lay-filter="area" lay-search=""><option value="">请选择</option>';
+                    for(var i = 0; i < json.rows.length; i++){
+                        str += '<option value="' + json.rows[i].id + '">' + json.rows[i].name + '</option>';
+                    }
+                    str += '</select></dd>';
+                    $("#lockParentSel").append(str);
+                    form.render('select');
+                }else{
+                    winui.window.msg(json.returnMessage, {icon: 2,time: 2000});
+                }
+            }});
+        }
+
+        //镇级行政区划
+        function loadChildAreaTownShip(){
+            AjaxPostUtil.request({url:reqBasePath + "commontarea004", params:{rowId: $("#addressAreaId").val()}, type:'json', method: "POST", callback:function(json){
+                if(json.returnCode == 0){
+                    var str = '<dd class="layui-col-xs3"><select id="townshipId" lay-filter="areaTownShip" lay-search=""><option value="">请选择</option>';
+                    for(var i = 0; i < json.rows.length; i++){
+                        str += '<option value="' + json.rows[i].id + '">' + json.rows[i].name + '</option>';
+                    }
+                    str += '</select></dd>';
+                    $("#lockParentSel").append(str);
+                    form.render('select');
+                }else{
+                    winui.window.msg(json.returnMessage, {icon: 2,time: 2000});
+                }
+            }});
+        }
 
         $("body").on("click", "#cancle", function(){
             parent.layer.close(index);
