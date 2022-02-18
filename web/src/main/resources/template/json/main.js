50724387336895fbcd94b016ad30197b23b41a3c
weizhiqiang <598748873@qq.com>
2022-02-18 21:46:20 +0800

    屏蔽部分功能

--- web/src/main/resources/template/json/main.js
+++ web/src/main/resources/template/json/main.js
@@ -2,6 +2,6 @@ sysMainMation = {
 	"mationTitle": "skyeye云办公 作者：卫志强", // 系统名称
 	"messageSysUrl": "http://127.0.0.1:8084/", // 消息系统页面请求地址
 	"webSocketPath": "ws://127.0.0.1:8081/", // 聊天socket-开发
-	"homePagePath": "http://127.0.0.1:80/", // 前台工程访问地址
-	"copyright": 'skyeye云系列 | Copyright © 2018~2021 | author：卫志强 | 开源版地址：<a href="https://gitee.com/doc_wei01/skyeye" target="_blank">skyeye</a>' // 版权信息
+	"homePagePath": "http://127.0.0.1:8080/", // 前台工程访问地址
+	"copyright": 'skyeye云系列 | Copyright © 2018~2022 | author：卫志强 | 开源版地址：<a href="https://gitee.com/doc_wei01/skyeye" target="_blank">skyeye</a>' // 版权信息
 };
