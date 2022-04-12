7e94546f6e70305232bd54ad235676b167940c48
weizhiqiang <598748873@qq.com>
2022-04-12 22:01:45 +0800

    工作流代码模板优化

--- web/src/main/resources/template/assets/lib/layui/customer/codeDocUtil.js
+++ web/src/main/resources/template/assets/lib/layui/customer/codeDocUtil.js
@@ -21,6 +21,10 @@ var codeDocUtil = {
         'selectColumn': /[$]{{selectColumn}}/g,
         'updateColumn': /[$]{{updateColumn}}/g,
         'javaBeanEntity': /[$]{{javaBeanEntity}}/g,
+        'addFormItem': /[$]{{addFormItem}}/g,
+        'editFormItem': /[$]{{editFormItem}}/g,
+        'detailsFormItem': /[$]{{detailsFormItem}}/g,
+        'submitFormData': /[$]{{submitFormData}}/g,
     },
 
     // 表参数数据
@@ -55,6 +59,10 @@ var codeDocUtil = {
         var selectColumn = []; // 查询时的表参数信息
         var updateColumn = []; // 编辑时的表参数信息
         var javaBeanEntity = []; // Java对象属性
+        var addFormItem = []; // 新增页面表单项
+        var editFormItem = []; // 编辑页面表单项
+        var detailsFormItem = []; // 详情页面表单项
+        var submitFormData = []; // 提交表单时的数据
         $.each(columns, function (i, item) {
             codeDocUtil.tableColumnData['table'] = item.tableName;
             var columnName = item.columnName;
@@ -84,6 +92,14 @@ var codeDocUtil = {
                 javaBeanEntity.push('@ApiModelProperty(value = "' + item.columnComment + '")\n\t' +
                     'private ' + codeDocUtil.getJavaAttr(item.columnType) + ' ' + lowerColumnName + '');
             }
+
+            if(lowerColumnName != 'id' && lowerColumnName != 'createTime' && lowerColumnName != 'createId'
+                && lowerColumnName != 'lastUpdateId' && lowerColumnName != 'lastUpdateTime') {
+                addFormItem.push(codeDocUtil.getAddFormItem(item.columnComment, lowerColumnName, item.isNullable));
+                editFormItem.push(codeDocUtil.getEditFormItem(item.columnComment, lowerColumnName, item.isNullable));
+                detailsFormItem.push(codeDocUtil.getDetailsFormItem(item.columnComment, lowerColumnName, item.isNullable));
+                submitFormData.push(lowerColumnName + ': $("#' + lowerColumnName + '").val()');
+            }
         });
         codeDocUtil.tableColumnData['currentTime'] = getFormatDate();
         codeDocUtil.tableColumnData['column'] = column.join(', ');
@@ -91,16 +107,65 @@ var codeDocUtil = {
         codeDocUtil.tableColumnData['selectColumn'] = selectColumn.join(',\n\t\t\t');
         codeDocUtil.tableColumnData['updateColumn'] = updateColumn.join(',\n\t\t\t');
         codeDocUtil.tableColumnData['javaBeanEntity'] = javaBeanEntity.join(';\n\t\n\t');
+        codeDocUtil.tableColumnData['addFormItem'] = addFormItem.join('');
+        codeDocUtil.tableColumnData['editFormItem'] = editFormItem.join('');
+        codeDocUtil.tableColumnData['detailsFormItem'] = detailsFormItem.join('');
+        codeDocUtil.tableColumnData['submitFormData'] = submitFormData.join(',\n\t\t\t') + ',';
     },
 
     getJavaAttr: function (columnType){
         if(columnType.indexOf('varchar') >= 0 || columnType.indexOf('datetime') >= 0 || columnType.indexOf('longtext') >= 0
-            || columnType.indexOf('decimal') >= 0){
+            || columnType.indexOf('decimal') >= 0) {
             return 'String';
         } else if(columnType.indexOf('int') >= 0 || columnType.indexOf('bigint') >= 0){
             return 'Integer';
         }
         return 'String';
+    },
+
+    getAddFormItem: function (title, name, isNullable){
+        if('NO' == isNullable){
+            return '<div class="layui-form-item layui-col-xs12">\n' +
+                '                <label class="layui-form-label">' + title + '<i class="red">*</i></label>\n' +
+                '                <div class="layui-input-block">\n' +
+                '                    <input type="text" id="' + name + '" name="' + name + '" win-verify="required" placeholder="请输入' + title + '" class="layui-input"/>\n' +
+                '                </div>\n' +
+                '            </div>\n\t\t\t';
+        } else{
+            return '<div class="layui-form-item layui-col-xs12">\n' +
+                '                <label class="layui-form-label">' + title + '</label>\n' +
+                '                <div class="layui-input-block">\n' +
+                '                    <input type="text" id="' + name + '" name="' + name + '" placeholder="请输入' + title + '" class="layui-input"/>\n' +
+                '                </div>\n' +
+                '            </div>\n\t\t\t';
+        }
+    },
+
+    getEditFormItem: function (title, name, isNullable){
+        if('NO' == isNullable){
+            return '<div class="layui-form-item layui-col-xs12">\n' +
+                '                <label class="layui-form-label">' + title + '<i class="red">*</i></label>\n' +
+                '                <div class="layui-input-block">\n' +
+                '                    <input type="text" id="' + name + '" name="' + name + '" win-verify="required" placeholder="请输入' + title + '" class="layui-input" value="{{' + name + '}}"/>\n' +
+                '                </div>\n' +
+                '            </div>\n\t\t\t';
+        } else{
+            return '<div class="layui-form-item layui-col-xs12">\n' +
+                '                <label class="layui-form-label">' + title + '</label>\n' +
+                '                <div class="layui-input-block">\n' +
+                '                    <input type="text" id="' + name + '" name="' + name + '" placeholder="请输入' + title + '" class="layui-input" value="{{' + name + '}}"/>\n' +
+                '                </div>\n' +
+                '            </div>\n\t\t\t';
+        }
+    },
+
+    getDetailsFormItem: function (title, name){
+        return '<div class="layui-form-item layui-col-xs12">\n' +
+            '                <label class="layui-form-label">' + title + '：</label>\n' +
+            '                <div class="layui-input-block">\n' +
+            '                    {{' + name + '}}\n' +
+            '                </div>\n' +
+            '            </div>\n\t\t\t';
     }
 
 }
