1d77753877b81b7ebf02c137a8b3774cd784fa37
weizhiqiang <598748873@qq.com>
2022-04-24 21:44:39 +0800

    招聘模块---转正申请完成

---
+++ boss/src/main/resources/template/js/bossInterviewRegularWorker/bossInterviewRegularWorkerList.js
@@ -0,0 +1,203 @@
+var rowId = "";
+
+// 转正申请
+layui.config({
+    base: basePath,
+    version: skyeyeVersion
+}).extend({
+    window: 'js/winui.window'
+}).define(['window', 'table', 'jquery', 'winui', 'form', 'laydate'], function (exports) {
+    winui.renderColor();
+    var $ = layui.$,
+        form = layui.form,
+        laydate = layui.laydate,
+        table = layui.table;
+
+    // 新增
+    authBtn('1650787545029');
+
+    laydate.render({
+        elem: '#createTime',
+        range: '~'
+    });
+
+    table.render({
+        id: 'messageTable',
+        elem: '#messageTable',
+        method: 'post',
+        url: flowableBasePath + 'queryBossInterviewRegularWorkerList',
+        where: getTableParams(),
+        even: true,
+        page: true,
+        limits: getLimits(),
+        limit: getLimit(),
+        cols: [[
+            { title: systemLanguage["com.skyeye.serialNumber"][languageType], type: 'numbers'},
+            { field: 'createName', title: '申请人', width: 140},
+            { field: 'departmentName', title: '转正部门', width: 140},
+            { field: 'jobName', title: '转正岗位', width: 150 },
+            { field: 'regularTime', title: '转正日期', align: 'center', width: 100 },
+            { field: 'processInstanceId', title: '流程ID', width: 100, templet: function(d){
+                return '<a lay-event="processDetails" class="notice-title-click">' + d.processInstanceId + '</a>';
+            }},
+            { field: 'stateName', title: '状态', width: 90, templet: function(d) {
+                return activitiUtil.showStateName2(d.state, 1);
+            }},
+            { field: 'createTime', title: systemLanguage["com.skyeye.createTime"][languageType], width: 150},
+            { title: systemLanguage["com.skyeye.operation"][languageType], fixed: 'right', align: 'center', width: 257, toolbar: '#messageTableBar'}
+        ]],
+        done: function(){
+            matchingLanguage();
+        }
+    });
+
+    table.on('tool(messageTable)', function (obj) {
+        var data = obj.data;
+        var layEvent = obj.event;
+        if (layEvent === 'details') { // 详情
+            details(data);
+        }else if (layEvent === 'edit') { // 编辑
+            edit(data);
+        }else if (layEvent === 'subApproval') { // 提交审批
+            subApproval(data);
+        }else if(layEvent === 'cancellation') { // 作废
+            cancellation(data);
+        }else if(layEvent === 'processDetails') { // 流程详情
+            activitiUtil.activitiDetails(data);
+        }else if(layEvent === 'revoke') { // 撤销申请
+            revoke(data);
+        }
+    });
+
+    // 添加
+    $("body").on("click", "#addBean", function(){
+        _openNewWindows({
+            url: "../../tpl/bossInterviewRegularWorker/bossInterviewRegularWorkerAdd.html",
+            title: systemLanguage["com.skyeye.addPageTitle"][languageType],
+            pageId: "bossInterviewRegularWorkerAdd",
+            area: ['90vw', '90vh'],
+            callBack: function(refreshCode){
+                if (refreshCode == '0') {
+                    winui.window.msg(systemLanguage["com.skyeye.successfulOperation"][languageType], {icon: 1,time: 2000});
+                    loadTable();
+                } else if (refreshCode == '-9999') {
+                    winui.window.msg(systemLanguage["com.skyeye.operationFailed"][languageType], {icon: 2,time: 2000});
+                }
+            }});
+    });
+
+    // 撤销
+    function revoke(data){
+        layer.confirm('确认撤销该申请吗？', { icon: 3, title: '撤销操作' }, function (index) {
+            layer.close(index);
+            AjaxPostUtil.request({url: flowableBasePath + "revokeBossInterviewRegularWorker", params: {processInstanceId: data.processInstanceId}, type: 'json', method: "PUT", callback: function(json){
+                if(json.returnCode == 0){
+                    winui.window.msg("提交成功", {icon: 1, time: 2000});
+                    loadTable();
+                }else{
+                    winui.window.msg(json.returnMessage, {icon: 2, time: 2000});
+                }
+            }});
+        });
+    }
+
+    // 编辑申请
+    function edit(data){
+        rowId = data.id;
+        _openNewWindows({
+            url: "../../tpl/bossInterviewRegularWorker/bossInterviewRegularWorkerEdit.html",
+            title: systemLanguage["com.skyeye.editPageTitle"][languageType],
+            pageId: "bossInterviewRegularWorkerEdit",
+            area: ['90vw', '90vh'],
+            callBack: function(refreshCode){
+                if (refreshCode == '0') {
+                    winui.window.msg(systemLanguage["com.skyeye.successfulOperation"][languageType], {icon: 1,time: 2000});
+                    loadTable();
+                } else if (refreshCode == '-9999') {
+                    winui.window.msg(systemLanguage["com.skyeye.operationFailed"][languageType], {icon: 2,time: 2000});
+                }
+            }
+        });
+    }
+
+    // 提交审批
+    function subApproval(data){
+        layer.confirm(systemLanguage["com.skyeye.approvalOperationMsg"][languageType], {icon: 3, title: systemLanguage["com.skyeye.approvalOperation"][languageType]}, function (index) {
+            layer.close(index);
+            activitiUtil.startProcess(sysActivitiModel["bossInterviewRegularWorker"]["key"], function (approvalId) {
+                var params = {
+                    id: data.id,
+                    approvalId: approvalId
+                };
+                AjaxPostUtil.request({url: flowableBasePath + "editBossInterviewRegularWorkerToSubApproval", params: params, type: 'json', method: "POST", callback: function(json){
+                    if(json.returnCode == 0){
+                        winui.window.msg("提交成功", {icon: 1, time: 2000});
+                        loadTable();
+                    }else{
+                        winui.window.msg(json.returnMessage, {icon: 2, time: 2000});
+                    }
+                }});
+            });
+        });
+    }
+
+    // 作废
+    function cancellation(data){
+        layer.confirm('确认作废该申请吗？', { icon: 3, title: '作废操作' }, function (index) {
+            layer.close(index);
+            AjaxPostUtil.request({url: flowableBasePath + "updateBossInterviewRegularWorkerToCancellation", params: {id: data.id}, type: 'json', method: "PUT", callback: function(json){
+                if(json.returnCode == 0){
+                    winui.window.msg(systemLanguage["com.skyeye.successfulOperation"][languageType], {icon: 1, time: 2000});
+                    loadTable();
+                }else{
+                    winui.window.msg(json.returnMessage, {icon: 2, time: 2000});
+                }
+            }});
+        });
+    }
+
+    // 详情
+    function details(data){
+        rowId = data.id;
+        _openNewWindows({
+            url: "../../tpl/bossInterviewRegularWorker/bossInterviewRegularWorkerDetails.html",
+            title: systemLanguage["com.skyeye.detailsPageTitle"][languageType],
+            pageId: "bossInterviewRegularWorkerDetails",
+            area: ['90vw', '90vh'],
+            callBack: function(refreshCode){
+            }
+        });
+    }
+
+    form.render();
+    form.on('submit(formSearch)', function (data) {
+        if (winui.verifyForm(data.elem)) {
+            table.reload("messageTable", {page: {curr: 1}, where: getTableParams()});
+        }
+        return false;
+    });
+
+    // 刷新
+    $("body").on("click", "#reloadTable", function(){
+        loadTable();
+    });
+
+    function loadTable(){
+        table.reload("messageTable", {where: getTableParams()});
+    }
+
+    function getTableParams(){
+        var startTime = "", endTime = "";
+        if(!isNull($("#createTime").val())){
+            startTime = $("#createTime").val().split('~')[0].trim() + ' 00:00:00';
+            endTime = $("#createTime").val().split('~')[1].trim() + ' 23:59:59';
+        }
+        return {
+            state: $("#state").val(),
+            startTime: startTime,
+            endTime: endTime
+        };
+    }
+
+    exports('bossInterviewRegularWorkerList', {});
+});
