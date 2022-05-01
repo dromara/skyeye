f8a9132654c577e49df46ef89a0d04dc91f322a3
weizhiqiang <598748873@qq.com>
2022-05-02 00:48:18 +0800

    业务流程规划新增基础信息

--- planpro/src/main/resources/template/js/planProject/planProjectList.js
+++ planpro/src/main/resources/template/js/planProject/planProjectList.js
@@ -27,20 +27,23 @@ layui.config({
 		limit: getLimit(),
 	    cols: [[
 	        { title: systemLanguage["com.skyeye.serialNumber"][languageType], type: 'numbers'},
-	        { field:'projectName', width:300, title: '项目名称'},
-            { field:'id', width:100, title: '项目简介', templet: function(d){
+	        { field: 'projectName', width:300, title: '项目名称'},
+            { field: 'id', width:100, title: '项目简介', templet: function(d){
 	        	return '<i class="fa fa-fw fa-html5 cursor" lay-event="projectDesc"></i>';
 	        }},
-	        { field:'id', width:100, title: '是否共享', templet: function(d){
-	        	if(d.isShare == '1'){
-	        		return '私人';
-	        	}else if(d.isShare == '2'){
-	        		return '公开分享';
-	        	}
+	        { field: 'id', width:100, title: '是否共享', templet: function(d){
+				if (d.isShare == '1') {
+					return '私人';
+				} else if (d.isShare == '2') {
+					return '公开分享';
+				}
 	        }},
-	        { field:'childsTypeOneNum', width: 120, title: '目录数量'},
-	        { field:'childsTypeTwoNum', width: 120, title: '流程图数量'},
-	        { field: 'createTime', title: systemLanguage["com.skyeye.createTime"][languageType], width: 180 },
+	        { field: 'childsTypeOneNum', width: 120, title: '目录数量'},
+	        { field: 'childsTypeTwoNum', width: 120, title: '流程图数量'},
+			{ field: 'createName', title: systemLanguage["com.skyeye.createName"][languageType], width: 140 },
+			{ field: 'createTime', title: systemLanguage["com.skyeye.createTime"][languageType], align: 'center', width: 150 },
+			{ field: 'lastUpdateName', title: systemLanguage["com.skyeye.lastUpdateName"][languageType], align: 'left', width: 140 },
+			{ field: 'lastUpdateTime', title: systemLanguage["com.skyeye.lastUpdateTime"][languageType], align: 'center', width: 150},
 	        { title: systemLanguage["com.skyeye.operation"][languageType], fixed: 'right', align: 'center', width: 240, toolbar: '#tableBar'}
 	    ]],
 	    done: function(){
@@ -63,7 +66,7 @@ layui.config({
 	            type: 1,
 	            title: '项目简介',
 	            shade: 0.3,
-	            area: ['1200px', '600px'],
+	            area: ['90vw', '90vh'],
 	            content: data.projectDesc,
 	        });
         }
@@ -141,7 +144,7 @@ layui.config({
 	form.render();
 	form.on('submit(formSearch)', function (data) {
 		if (winui.verifyForm(data.elem)) {
-			refreshTable();
+			table.reload("messageTable", {page: {curr: 1}, where: getTableParams()});
 		}
 		return false;
 	});
@@ -153,10 +156,6 @@ layui.config({
 
     function loadTable(){
     	table.reload("messageTable", {where: getTableParams()});
-    }
-    
-    function refreshTable(){
-    	table.reload("messageTable", {page: {curr: 1}, where: getTableParams()});
     }
 
     function getTableParams(){
