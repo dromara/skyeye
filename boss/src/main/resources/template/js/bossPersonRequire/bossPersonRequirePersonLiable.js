e6e4de1e51de169c52c32d5da36997e661d29f83
weizhiqiang <598748873@qq.com>
2022-05-06 11:39:29 +0800

    细节性提示修改

--- boss/src/main/resources/template/js/bossPersonRequire/bossPersonRequirePersonLiable.js
+++ boss/src/main/resources/template/js/bossPersonRequire/bossPersonRequirePersonLiable.js
@@ -99,5 +99,9 @@ layui.config({
         $("body").on("click", ".enclosureItem", function(){
             download(fileBasePath + $(this).attr("rowpath"), $(this).html());
         });
+
+        $("body").on("click", "#cancle", function() {
+            parent.layer.close(index);
+        });
     });
 });
