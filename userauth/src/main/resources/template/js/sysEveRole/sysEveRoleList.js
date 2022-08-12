3a50126c9e3bfb356492ed0db0d86080393f820a
weizhiqiang <>
2022-08-12 14:12:25 +0800

    【角色管理】解决刷新列表闪烁的问题

--- userauth/src/main/resources/template/js/sysEveRole/sysEveRoleList.js
+++ userauth/src/main/resources/template/js/sysEveRole/sysEveRoleList.js
@@ -35,7 +35,7 @@ layui.config({
 	    done: function(json) {
 	    	matchingLanguage();
 			initTableSearchUtil.initAdvancedSearch(this, json.searchFilter, form, "请输入角色名称", function () {
-				table.reload("messageTable", {page: {curr: 1}, where: getTableParams()});
+				table.reloadData("messageTable", {page: {curr: 1}, where: getTableParams()});
 			});
 	    }
 	});
@@ -121,7 +121,7 @@ layui.config({
 	});
 
     function loadTable(){
-    	table.reload("messageTable", {where: getTableParams()});
+    	table.reloadData("messageTable", {where: getTableParams()});
     }
 
     function getTableParams() {
