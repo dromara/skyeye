e3041f1c5a9c9897fae231492bcf5afb14840d92
weizhiqiang <598748873@qq.com>
2022-09-26 22:14:57 +0800

    获取我申领的未使用的配件列表修改，待验证---请输入剩余数量TODO待完善插件

--- seal-service/src/main/resources/template/js/myparts/mypartslist.js
+++ seal-service/src/main/resources/template/js/myparts/mypartslist.js
@@ -10,27 +10,30 @@ layui.config({
 		form = layui.form,
 		table = layui.table;
 	
-	//获取我申领的未使用的配件
+	// 获取我申领的未使用的配件
 	table.render({
 	    id: 'messageTable',
 	    elem: '#messageTable',
 	    method: 'post',
 	    url: flowableBasePath + 'sealseservice031',
-	    where: {materialName: $("#materialName").val(), materialModel: $("#materialModel").val()},
+	    where: getTableParams(),
 	    even: true,
 	    page: true,
-	    limits: [8, 16, 24, 32, 40, 48, 56],
-	    limit: 8,
+		limits: getLimits(),
+		limit: getLimit(),
 	    cols: [[
 	        { title: systemLanguage["com.skyeye.serialNumber"][languageType], type: 'numbers' },
 	        { field: 'materialName', title: '配件名称', align: 'left', width: 200 },
 	        { field: 'unitName', title: '计量单位', align: 'left', width: 80 },
 	        { field: 'materialModel', title: '配件规格', align: 'left', width: 150 },
-	        { field: 'unitPrice', title: '单价', align: 'left', width: 100 },
+	        { field: 'normsFormtRetailPrice', title: '单价', align: 'left', width: 100 },
 	        { field: 'operNumber', title: '剩余数量', align: 'left', width: 100 }
 	    ]],
 	    done: function(json) {
 	    	matchingLanguage();
+			initTableSearchUtil.initAdvancedSearch(this, json.searchFilter, form, "请输入剩余数量TODO待完善插件", function () {
+				table.reloadData("messageTable", {page: {curr: 1}, where: getTableParams()});
+			});
 	    }
 	});
 	
@@ -40,29 +43,17 @@ layui.config({
     });
 	
 	form.render();
-	
-	//刷新数据
 	$("body").on("click", "#reloadTable", function() {
 		loadTable();
 	});
 	
-	form.render();
-	
-	form.on('submit(formSearch)', function (data) {
-    	
-        if (winui.verifyForm(data.elem)) {
-        	refreshTable();
-        }
-        return false;
-	});
-    
     function loadTable() {
-    	table.reloadData("messageTable", {where: {materialName: $("#materialName").val(), materialModel: $("#materialModel").val()}});
+    	table.reloadData("messageTable", {where: getTableParams()});
     }
     
-    function refreshTable(){
-    	table.reloadData("messageTable", {page: {curr: 1}, where: {materialName: $("#materialName").val(), materialModel: $("#materialModel").val()}});
-    }
+    function getTableParams() {
+		return $.extend(true, {}, initTableSearchUtil.getSearchValue("messageTable"));
+	}
 
     exports('mypartslist', {});
 });
