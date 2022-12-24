1684361c08be6b098d5dad169a08d79e4da61560
weizhiqiang <598748873@qq.com>
2022-12-24 21:18:09 +0800

    删除工作流属性展示

--- base-server/src/main/resources/template/js/processAttr/processAttrList.js
+++ base-server/src/main/resources/template/js/processAttr/processAttrList.js
@@ -71,7 +71,7 @@ layui.config({
 	function del(data, obj) {
 		layer.confirm(systemLanguage["com.skyeye.deleteOperationMsg"][languageType], {icon: 3, title: systemLanguage["com.skyeye.deleteOperation"][languageType]}, function (index) {
 			layer.close(index);
-			AjaxPostUtil.request({url: reqBasePath + "deleteTeamTemplateById", params: {id: data.id}, type: 'json', method: "DELETE", callback: function (json) {
+			AjaxPostUtil.request({url: reqBasePath + "deleteAttrTransformById", params: {id: data.id}, type: 'json', method: "DELETE", callback: function (json) {
 				winui.window.msg(systemLanguage["com.skyeye.deleteOperationSuccessMsg"][languageType], {icon: 1, time: 2000});
 				loadTable();
 			}});
