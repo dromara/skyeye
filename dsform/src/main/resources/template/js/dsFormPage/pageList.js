d4505c1e8cf5a3044a296a861bc8e540f4e95803
weizhiqiang <598748873@qq.com>
2023-01-08 21:44:31 +0800

    删除编码配置功能

--- dsform/src/main/resources/template/js/dsFormPage/pageList.js
+++ dsform/src/main/resources/template/js/dsFormPage/pageList.js
@@ -27,8 +27,12 @@ layui.config({
 	        { title: systemLanguage["com.skyeye.serialNumber"][languageType], type: 'numbers' },
 	        { field: 'name', title: '名称', align: 'left', width: 120 },
 	        { field: 'remark', title: '简介', align: 'left', width: 350 },
-	        { field: 'numCode', title: '编号', align: 'center', width: 150 },
-	        { title: systemLanguage["com.skyeye.operation"][languageType], fixed: 'right', align: 'center', width: 200, toolbar: '#tableBar'}
+	        { field: 'numCode', title: '编号', width: 150 },
+			{ field: 'createName', title: systemLanguage["com.skyeye.createName"][languageType], width: 120 },
+			{ field: 'createTime', title: systemLanguage["com.skyeye.createTime"][languageType], align: 'center', width: 150 },
+			{ field: 'lastUpdateName', title: systemLanguage["com.skyeye.lastUpdateName"][languageType], align: 'left', width: 120 },
+			{ field: 'lastUpdateTime', title: systemLanguage["com.skyeye.lastUpdateTime"][languageType], align: 'center', width: 150 },
+	        { title: systemLanguage["com.skyeye.operation"][languageType], fixed: 'right', align: 'center', width: 200, toolbar: '#tableBar' }
 	    ]],
 	    done: function(json) {
 	    	matchingLanguage();
