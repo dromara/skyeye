e445de01a60f77be74d60a249d5bc2e92ed13fdd
weizhiqiang <598748873@qq.com>
2023-06-18 22:01:02 +0800

    我的用品领用历史重构完成

--- admin-assistant/src/main/resources/template/js/myHasMation/myAssetArticlesManagement.js
+++ admin-assistant/src/main/resources/template/js/myHasMation/myAssetArticlesManagement.js
@@ -1,6 +1,4 @@
 
-var rowId = "";
-
 layui.config({
 	base: basePath, 
 	version: skyeyeVersion
@@ -12,63 +10,49 @@ layui.config({
 		form = layui.form,
 		table = layui.table;
 
-	showList();
 	// 我的用品领用历史列表
-	function showList(){
-		table.render({
-		    id: 'messageTable',
-		    elem: '#messageTable',
-		    method: 'post',
-		    url: flowableBasePath + 'myhasmation004',
-		    where: getTableParams(),
-		    even: true,
-		    page: true,
-		    limits: getLimits(),
-	    	limit: getLimit(),
-		    cols: [[
-		        { title: systemLanguage["com.skyeye.serialNumber"][languageType], rowspan: '3', type: 'numbers' },
-				{ field: 'name', title: '用品名称', align: 'left', rowspan: '3', width: 120 },
-				{ field: 'articlesNum', title: '用品编号', align: 'left', rowspan: '3', width: 200 },
-				{ field: 'specifications', title: '用品规格', align: 'left', rowspan: '3', width: 80 },
-				{ field: 'typeName', title: '用品类型', align: 'left', rowspan: '3', width: 120 },
-				{ field: 'applyUseNum', title: '领用数量', align: 'left', rowspan: '3', width: 80 },
-				{ title: '所属单据', align: 'center', colspan: '3'}
-		    ],[
-				{ field: 'oddTitle', title: '单据标题', align: 'left', width: 240},
-				{ field: 'oddNumber', title: '单据编号', align: 'center', width: 180},
-				{ field: 'oddCreateTime', title: '单据日期', align: 'center', width: 140}
-			]],
-		    done: function(json) {
-		    	matchingLanguage();
-		    }
-		});
-	}
-
-    $("body").on("click", "#reloadmessageTable", function() {
-    	loadTable();
-    });
-	form.render();
-	form.on('submit(formSearch)', function (data) {
-		if (winui.verifyForm(data.elem)) {
-			refreshTable();
+	table.render({
+		id: 'messageTable',
+		elem: '#messageTable',
+		method: 'post',
+		url: sysMainMation.admBasePath + 'myhasmation004',
+		where: getTableParams(),
+		even: true,
+		page: true,
+		limits: getLimits(),
+		limit: getLimit(),
+		cols: [[
+			{ title: systemLanguage["com.skyeye.serialNumber"][languageType], rowspan: '3', type: 'numbers' },
+			{ field: 'name', title: '用品名称', align: 'left', rowspan: '3', width: 120 },
+			{ field: 'articlesNum', title: '用品编号', align: 'left', rowspan: '3', width: 200 },
+			{ field: 'specifications', title: '用品规格', align: 'left', rowspan: '3', width: 80 },
+			{ field: 'typeName', title: '用品类型', align: 'left', rowspan: '3', width: 120 },
+			{ field: 'applyUseNum', title: '领用数量', align: 'left', rowspan: '3', width: 80 },
+			{ title: '所属单据', align: 'center', colspan: '3'}
+		],[
+			{ field: 'title', title: '单据标题', align: 'left', width: 240},
+			{ field: 'oddNumber', title: '单据编号', align: 'center', width: 180},
+			{ field: 'createTime', title: '单据日期', align: 'center', width: 140}
+		]],
+		done: function(json) {
+			matchingLanguage();
+			initTableSearchUtil.initAdvancedSearch(this, json.searchFilter, form, "请输入单号，用品名称", function () {
+				table.reloadData("messageTable", {page: {curr: 1}, where: getTableParams()});
+			});
 		}
-		return false;
 	});
 
-    function loadTable() {
-    	table.reloadData("messageTable", {where: getTableParams()});
-    }
-
-	function refreshTable(){
-		table.reloadData("messageTable", {page: {curr: 1}, where: getTableParams()});
+	form.render();
+	$("body").on("click", "#reloadTable", function() {
+		loadTable();
+	});
+	function loadTable() {
+		table.reloadData("messageTable", {where: getTableParams()});
 	}
-    
-    function getTableParams() {
-    	return {
-			articlesName: $("#articlesName").val(),
-			articlesNum: $("#articlesNum").val()
-    	};
-    }
+
+	function getTableParams() {
+		return $.extend(true, {}, initTableSearchUtil.getSearchValue("messageTable"));
+	}
     
     exports('myAssetArticlesManagement', {});
 });
