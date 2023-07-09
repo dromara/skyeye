86debcc33286544d82f98f3000ed271e4ff0ea7e
weizhiqiang <598748873@qq.com>
2023-07-09 14:25:27 +0800

    请假申请托管到表单布局

--- checkwork/src/main/resources/template/js/checkWorkLeave/checkWorkLeaveList.js
+++ checkwork/src/main/resources/template/js/checkWorkLeave/checkWorkLeaveList.js
@@ -1,33 +1,24 @@
-var rowId = "";
-
-var taskType = "";//流程详情的主标题
-var processInstanceId = "";//流程id
 
 layui.config({
     base: basePath,
     version: skyeyeVersion
 }).extend({
     window: 'js/winui.window'
-}).define(['window', 'table', 'jquery', 'winui', 'form', 'laydate'], function (exports) {
+}).define(['window', 'table', 'jquery', 'winui', 'form'], function (exports) {
     winui.renderColor();
     var $ = layui.$,
         form = layui.form,
-        laydate = layui.laydate,
         table = layui.table;
     var serviceClassName = sysServiceMation["checkWorkLeave"]["key"];
 
-    // 新增请假申请
     authBtn('1616239712898');
-
-    // 申请时间
-    laydate.render({elem: '#applyTime', range: '~'});
 
     // 我的请假申请列表
     table.render({
         id: 'messageTable',
         elem: '#messageTable',
         method: 'post',
-        url: flowableBasePath + 'checkworkleave001',
+        url: sysMainMation.checkworkBasePath + 'checkworkleave001',
         where: getTableParams(),
         even: true,
         page: true,
@@ -35,29 +26,35 @@ layui.config({
         limit: getLimit(),
         cols: [[
             { title: systemLanguage["com.skyeye.serialNumber"][languageType], type: 'numbers' },
-            { field: 'title', title: '标题', width: 300, templet: function (d) {
-                return '<a lay-event="dedails" class="notice-title-click">' + d.title + '</a>';
+            { field: 'oddNumber', title: '单号', width: 200, align: 'center', templet: function (d) {
+                return '<a lay-event="details" class="notice-title-click">' + d.oddNumber + '</a>';
             }},
-            { field: 'oddNum', title: '单号', width: 200 },
-            { field: 'processInstanceId', title: '流程ID', width: 100, templet: function (d) {
+            { field: 'name', title: '标题', width: 300 },
+            { field: 'processInstanceId', title: '流程ID', width: 80, align: 'center', templet: function (d) {
                 return '<a lay-event="processDetails" class="notice-title-click">' + d.processInstanceId + '</a>';
             }},
-            { field: 'stateName', title: '状态', width: 90, templet: function (d) {
-                return activitiUtil.showStateName2(d.state, 1);
+            { field: 'state', title: '状态', width: 90, align: 'center', templet: function (d) {
+                return skyeyeClassEnumUtil.getEnumDataNameByCodeAndKey("flowableStateEnum", 'id', d.state, 'name');
             }},
-            { field: 'createTime', title: systemLanguage["com.skyeye.createTime"][languageType], width: 150 },
+            { field: 'createName', title: systemLanguage["com.skyeye.createName"][languageType], width: 120 },
+            { field: 'createTime', title: systemLanguage["com.skyeye.createTime"][languageType], align: 'center', width: 150 },
+            { field: 'lastUpdateName', title: systemLanguage["com.skyeye.lastUpdateName"][languageType], align: 'left', width: 120 },
+            { field: 'lastUpdateTime', title: systemLanguage["com.skyeye.lastUpdateTime"][languageType], align: 'center', width: 150 },
             { title: systemLanguage["com.skyeye.operation"][languageType], fixed: 'right', align: 'center', width: 257, toolbar: '#messageTableBar'}
         ]],
         done: function(json) {
             matchingLanguage();
+            initTableSearchUtil.initAdvancedSearch(this, json.searchFilter, form, "请输入单号，标题", function () {
+                table.reloadData("messageTable", {page: {curr: 1}, where: getTableParams()});
+            });
         }
     });
 
     table.on('tool(messageTable)', function (obj) {
         var data = obj.data;
         var layEvent = obj.event;
-        if (layEvent === 'dedails') { // 详情
-            dedails(data);
+        if (layEvent === 'details') { // 详情
+            details(data);
         } else if (layEvent === 'edit') { // 编辑
             edit(data);
         } else if (layEvent === 'subApproval') { // 提交审批
@@ -74,7 +71,7 @@ layui.config({
     // 新增请假申请
     $("body").on("click", "#addBean", function() {
         _openNewWindows({
-            url: "../../tpl/checkWorkLeave/checkWorkLeaveAdd.html",
+            url: systemCommonUtil.getUrl('FP2023070400001', null),
             title: "请假申请",
             pageId: "checkWorkLeaveAdd",
             area: ['90vw', '90vh'],
@@ -88,7 +85,7 @@ layui.config({
     function revoke(data) {
         layer.confirm('确认撤销该申请吗？', { icon: 3, title: '撤销操作' }, function (index) {
             layer.close(index);
-            AjaxPostUtil.request({url: flowableBasePath + "checkworkleave009", params: {processInstanceId: data.processInstanceId}, type: 'json', method: "PUT", callback: function (json) {
+            AjaxPostUtil.request({url: sysMainMation.checkworkBasePath + "checkworkleave009", params: {processInstanceId: data.processInstanceId}, type: 'json', method: "PUT", callback: function (json) {
                 winui.window.msg("提交成功", {icon: 1, time: 2000});
                 loadTable();
             }});
@@ -97,9 +94,8 @@ layui.config({
 
     // 编辑请假申请
     function edit(data) {
-        rowId = data.id;
         _openNewWindows({
-            url: "../../tpl/checkWorkLeave/checkWorkLeaveEdit.html",
+            url: systemCommonUtil.getUrl('FP2023070400002&id=' + data.id, null),
             title: "请假申请",
             pageId: "checkWorkLeaveEdit",
             area: ['90vw', '90vh'],
@@ -116,10 +112,10 @@ layui.config({
             layer.close(index);
             activitiUtil.startProcess(serviceClassName, null, function (approvalId) {
                 var params = {
-                    rowId: data.id,
+                    id: data.id,
                     approvalId: approvalId
                 };
-                AjaxPostUtil.request({url: flowableBasePath + "checkworkleave006", params: params, type: 'json', callback: function (json) {
+                AjaxPostUtil.request({url: sysMainMation.checkworkBasePath + "checkworkleave006", params: params, type: 'json', method: "POST", callback: function (json) {
                     winui.window.msg("提交成功", {icon: 1, time: 2000});
                     loadTable();
                 }});
@@ -131,7 +127,7 @@ layui.config({
     function cancellation(data) {
         layer.confirm('确认作废该申请吗？', { icon: 3, title: '作废操作' }, function (index) {
             layer.close(index);
-            AjaxPostUtil.request({url: flowableBasePath + "checkworkleave007", params: {rowId: data.id}, type: 'json', callback: function (json) {
+            AjaxPostUtil.request({url: sysMainMation.checkworkBasePath + "checkworkleave007", params: {id: data.id}, type: 'json', method: "POST", callback: function (json) {
                 winui.window.msg(systemLanguage["com.skyeye.successfulOperation"][languageType], {icon: 1, time: 2000});
                 loadTable();
             }});
@@ -139,10 +135,9 @@ layui.config({
     }
 
     // 请假申请详情
-    function dedails(data) {
-        rowId = data.id;
+    function details(data) {
         _openNewWindows({
-            url: "../../tpl/checkWorkLeave/checkWorkLeaveDetails.html",
+            url: systemCommonUtil.getUrl('FP2023070400003&id=' + data.id, null),
             title: systemLanguage["com.skyeye.detailsPageTitle"][languageType],
             pageId: "checkWorkLeaveDetails",
             area: ['90vw', '90vh'],
@@ -152,34 +147,15 @@ layui.config({
     }
 
     form.render();
-    form.on('submit(formSearch)', function (data) {
-        if (winui.verifyForm(data.elem)) {
-            table.reloadData("messageTable", {page: {curr: 1}, where: getTableParams()});
-        }
-        return false;
-    });
-
-    // 刷新
     $("body").on("click", "#reloadTable", function() {
         loadTable();
     });
-
-    // 刷新数据
     function loadTable() {
         table.reloadData("messageTable", {where: getTableParams()});
     }
 
     function getTableParams() {
-        var startTime = "", endTime = "";
-        if (!isNull($("#applyTime").val())) {
-            startTime = $("#applyTime").val().split('~')[0].trim() + ' 00:00:00';
-            endTime = $("#applyTime").val().split('~')[1].trim() + ' 23:59:59';
-        }
-        return {
-            state: $("#state").val(),
-            startTime: startTime,
-            endTime: endTime
-        };
+        return $.extend(true, {}, initTableSearchUtil.getSearchValue("messageTable"));
     }
 
     exports('checkWorkLeaveList', {});
