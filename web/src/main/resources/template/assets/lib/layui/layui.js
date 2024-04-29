3de9e56849c82f8b4ba2b2d0d9d499f1d56a6c50
weizhiqiang <598748873@qq.com>
2024-04-29 10:28:43 +0800

    feat: 新增公文模版枚举类

--- web/src/main/resources/template/assets/lib/layui/layui.js
+++ web/src/main/resources/template/assets/lib/layui/layui.js
@@ -63,6 +63,7 @@ var dataTypeList = [
 	{id: 33, name: "学校-科目"},
 	{id: 34, name: "知识库-文件上传(正常情况下会被删除掉)"},
 	{id: 38, name: "印章Logo"},
+	{id: 39, name: "公文模版"},
 ];
 
 //系统cookies从哪里获取， true：从用户登陆获取；false：跨域获取
