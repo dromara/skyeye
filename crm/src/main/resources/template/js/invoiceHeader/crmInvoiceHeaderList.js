d5a84525abea344345d59622d27b0cedbf19e84f
weizhiqiang <598748873@qq.com>
2024-05-03 18:34:07 +0800

    feat: 新增客户发票抬头管理

--- crm/src/main/resources/template/js/invoiceHeader/crmInvoiceHeaderList.js
+++ crm/src/main/resources/template/js/invoiceHeader/crmInvoiceHeaderList.js
@@ -19,13 +19,13 @@ layui.config({
         return false;
     }
 
-    var authPermission = teamObjectPermissionUtil.checkTeamBusinessAuthPermission(objectId, 'crmFollowUpAuthEnum');
+    var authPermission = teamObjectPermissionUtil.checkTeamBusinessAuthPermission(objectId, 'crmInvoiceHeaderAuthEnum');
 
     table.render({
         id: 'messageTable',
         elem: '#messageTable',
         method: 'post',
-        url: sysMainMation.crmBasePath + 'queryFollowUpList',
+        url: sysMainMation.crmBasePath + 'queryInvoiceHeaderList',
         where: getTableParams(),
         even: true,
         page: true,
@@ -33,25 +33,14 @@ layui.config({
         limit: getLimit(),
         cols: [[
             { title: systemLanguage["com.skyeye.serialNumber"][languageType], type: 'numbers' },
-            { field: 'oddNumber', title: '回访编号', align: 'left', width: 150, templet: function (d) {
-                return '<a lay-event="details" class="notice-title-click">' + d.oddNumber + '</a>';
+            { field: 'name', title: '发票抬头', align: 'left', width: 150, templet: function (d) {
+                return '<a lay-event="details" class="notice-title-click">' + d.name + '</a>';
             }},
-            { field: 'contractId', title: '合同', width: 200, templet: function (d) {
-                return getNotUndefinedVal(d.contractMation?.title);
-            }},
-            { field: 'typeId', title: '类型', align: 'center', width: 120, templet: function (d) {
-                return sysDictDataUtil.getDictDataNameByCodeAndKey('FOLLOW_UP_TYPE', d.typeId);
-            }},
-            { field: 'contacts', title: '联系人', width: 120, templet: function (d) {
-                return getNotUndefinedVal(d.contactsMation?.name);
-            }},
-            { field: 'satisfaction', title: '客户满意度', align: 'center', width: 120, templet: function (d) {
-                return sysDictDataUtil.getDictDataNameByCodeAndKey('FOLLOW_UP_SATISFACTION', d.satisfaction);
-            }},
-            { field: 'followUserId', title: '回访人', width: 120, templet: function (d) {
-                return getNotUndefinedVal(d.followUserMation?.name);
-            }},
-            { field: 'followTime', title: '回访时间', align: 'center', width: 100 },
+            { field: 'identificationNumber', title: '纳税识别号', width: 150 },
+            { field: 'openingBank', title: '开户行', width: 150 },
+            { field: 'openingAccount', title: '开户帐号', width: 150 },
+            { field: 'billingAddress', title: '开票地址', width: 200 },
+            { field: 'phone', title: '电话', width: 120 },
             { field: 'createName', title: systemLanguage["com.skyeye.createName"][languageType], align: 'left', width: 120 },
             { field: 'createTime', title: systemLanguage["com.skyeye.createTime"][languageType], align: 'center', width: 150 },
             { field: 'lastUpdateName', title: systemLanguage["com.skyeye.lastUpdateName"][languageType], align: 'left', width: 120 },
@@ -69,7 +58,7 @@ layui.config({
         ]],
         done: function(json) {
             matchingLanguage();
-            initTableSearchUtil.initAdvancedSearch(this, json.searchFilter, form, "请输入回访编号", function () {
+            initTableSearchUtil.initAdvancedSearch(this, json.searchFilter, form, "请输入发票抬头，纳税识别号", function () {
                 table.reloadData("messageTable", {page: {curr: 1}, where: getTableParams()});
             });
         }
@@ -87,13 +76,12 @@ layui.config({
         }
     });
 
-
     // 新增
     $("body").on("click", "#addBean", function() {
         parent._openNewWindows({
-            url: systemCommonUtil.getUrl('FP2024050200001&objectId=' + objectId + '&objectKey=' + objectKey, null),
+            url: systemCommonUtil.getUrl('FP2024050300002&objectId=' + objectId + '&objectKey=' + objectKey, null),
             title: systemLanguage["com.skyeye.addPageTitle"][languageType],
-            pageId: "crmFollowAdd",
+            pageId: "crmInvoiceHeaderAdd",
             area: ['90vw', '90vh'],
             callBack: function (refreshCode) {
                 winui.window.msg(systemLanguage["com.skyeye.successfulOperation"][languageType], {icon: 1, time: 2000});
@@ -104,9 +92,9 @@ layui.config({
     // 编辑
     function edit(data) {
         parent._openNewWindows({
-            url: systemCommonUtil.getUrl('FP2024050200002&objectId=' + objectId + '&objectKey=' + objectKey + '&id=' + data.id, null),
+            url: systemCommonUtil.getUrl('FP2024050300003&objectId=' + objectId + '&objectKey=' + objectKey + '&id=' + data.id, null),
             title: systemLanguage["com.skyeye.editPageTitle"][languageType],
-            pageId: "crmFollowEdit",
+            pageId: "crmInvoiceHeaderEdit",
             area: ['90vw', '90vh'],
             callBack: function (refreshCode) {
                 winui.window.msg(systemLanguage["com.skyeye.successfulOperation"][languageType], {icon: 1, time: 2000});
@@ -117,9 +105,9 @@ layui.config({
     // 详情
     function details(data) {
         parent._openNewWindows({
-            url: systemCommonUtil.getUrl('FP2024050200003&objectId=' + objectId + '&objectKey=' + objectKey + '&id=' + data.id, null),
+            url: systemCommonUtil.getUrl('FP2024050300004&objectId=' + objectId + '&objectKey=' + objectKey + '&id=' + data.id, null),
             title: systemLanguage["com.skyeye.detailsPageTitle"][languageType],
-            pageId: "crmFollowDetails",
+            pageId: "crmInvoiceHeaderDetails",
             area: ['90vw', '90vh'],
             callBack: function (refreshCode) {
                 winui.window.msg(systemLanguage["com.skyeye.successfulOperation"][languageType], {icon: 1, time: 2000});
@@ -131,7 +119,7 @@ layui.config({
     function del(data, obj) {
         layer.confirm(systemLanguage["com.skyeye.deleteOperationMsg"][languageType], {icon: 3, title: systemLanguage["com.skyeye.deleteOperation"][languageType]}, function (index) {
             layer.close(index);
-            AjaxPostUtil.request({url: sysMainMation.crmBasePath + "deleteFollowUpById", params: {id: data.id}, type: 'json', method: 'DELETE', callback: function (json) {
+            AjaxPostUtil.request({url: sysMainMation.crmBasePath + "deleteInvoiceHeaderById", params: {id: data.id}, type: 'json', method: 'DELETE', callback: function (json) {
                 winui.window.msg(systemLanguage["com.skyeye.deleteOperationSuccessMsg"][languageType], {icon: 1, time: 2000});
                 loadTable();
             }});
@@ -150,5 +138,5 @@ layui.config({
         return $.extend(true, {objectKey: objectKey, objectId: objectId}, initTableSearchUtil.getSearchValue("messageTable"));
     }
 
-    exports('crmFollowList', {});
+    exports('crmInvoiceHeaderList', {});
 });
