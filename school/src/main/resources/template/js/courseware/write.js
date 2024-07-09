42e24c06fd554c442642eefa0c0476d94b420ddc
xqz <2995480165@qq.com>
2024-07-09 21:41:29 +0800

    update:互动课件的增删改查，作业管理的增删改查

--- school/src/main/resources/template/js/courseware/write.js
+++ school/src/main/resources/template/js/courseware/write.js
@@ -1,7 +1,7 @@
 
 // 以下两个参数开启团队权限时有值
 var objectId = '', objectKey = '';
-
+var objectParams = {};
 // 根据以下两个参数判断：工作流的判断是否要根据serviceClassName的判断
 var serviceClassName;
 
@@ -17,34 +17,34 @@ layui.config({
     var id = GetUrlParam("id");
     var objectId = GetUrlParam("objectId");
     var objectKey = GetUrlParam("objectKey");
-    var subjectClassesId = GetUrlParam("subjectClassesId");
-
+    objectParams.subjectClassesId = GetUrlParam("subjectClassesId");
 
     if (isNull(id)) {
-        dsFormUtil.initAddPageForStatic('content', 'FP2023082800010', {
+        dsFormUtil.initAddPageForStatic('content', 'FP2024070900003', {
             savePreParams: function (params) {
                 params.objectId = objectId;
                 params.objectKey = objectKey;
-                params.subjectClassesId = subjectClassesId;
+                params.subjectClassesId = objectParams.subjectClassesId;
             }
         });
-    } else {
+    }else{
         AjaxPostUtil.request({
-            url: sysMainMation.schoolBasePath + "queryChapterById",
+            url: sysMainMation.schoolBasePath + "queryCoursewareById",
             params: {id: id},
             type: 'json',
             method: 'GET',
             callback: function (json) {
                 let data = json.bean;
-                dsFormUtil.initEditPageForStatic('content', 'FP2023082800011', data, {
+                dsFormUtil.initEditPageForStatic('content', 'FP2024070900004', data, {
                     savePreParams: function (params) {
                         params.objectId = objectId;
                         params.objectKey = objectKey;
-                        params.subjectClassesId = subjectClassesId;
+                        params.subjectClassesId = objectParams.subjectClassesId;
                     }
                 });
             }
         });
+
     }
 
 });
