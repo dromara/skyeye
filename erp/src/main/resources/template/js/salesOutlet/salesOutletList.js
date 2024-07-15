abd1e1008263cae4bbde6461b7fd7ab61be47874
LAPTOP-UV1MNL38\18023 <1802330370@qq.com>
2024-07-15 11:03:18 +0800

    feat:修改erp和生产物料管理的状态枚举

--- erp/src/main/resources/template/js/salesOutlet/salesOutletList.js
+++ erp/src/main/resources/template/js/salesOutlet/salesOutletList.js
@@ -41,7 +41,7 @@ layui.config({
                 return '<a lay-event="processDetails" class="notice-title-click">' + getNotUndefinedVal(d.processInstanceId) + '</a>';
             }},
             { field: 'state', title: '状态', rowspan: '2', width: 90, templet: function (d) {
-                return skyeyeClassEnumUtil.getEnumDataNameByCodeAndKey("erpOrderStateEnum", 'id', d.state, 'name');
+                return skyeyeClassEnumUtil.getEnumDataNameByCodeAndKey("flowableStateEnum", 'id', d.state, 'name');
             }},
             { field: 'createName', title: systemLanguage["com.skyeye.createName"][languageType], rowspan: '2', width: 120 },
             { field: 'createTime', title: systemLanguage["com.skyeye.createTime"][languageType], rowspan: '2', align: 'center', width: 150 },
