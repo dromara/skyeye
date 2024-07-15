90195c21f54bad6b72bd296a51bf42c19a7709d1
LAPTOP-UV1MNL38\18023 <1802330370@qq.com>
2024-07-15 16:41:41 +0800

    feat:新增工序验收、物料接收、物料退货单

---
+++ erp-produce/src/main/resources/template/js/materialReturnOrder/materialReturnOrderList.js
@@ -0,0 +1,74 @@
+
+layui.config({
+    base: basePath,
+    version: skyeyeVersion
+}).extend({
+    window: 'js/winui.window'
+}).define(['window', 'table', 'jquery', 'winui', 'form'], function (exports) {
+    winui.renderColor();
+    var $ = layui.$,
+        form = layui.form,
+        table = layui.table;
+
+    //物料退货单
+    table.render({
+        id: 'messageTable',
+        elem: '#messageTable',
+        method: 'post',
+        url: sysMainMation.erpBasePath + 'queryConfirmReturnList',
+        where: getTableParams(),
+        even: true,
+        page: true,
+        limits: getLimits(),
+        limit: getLimit(),
+        cols: [[
+            { title: systemLanguage["com.skyeye.serialNumber"][languageType], type: 'numbers', rowspan: '2' },
+            { field: 'oddNumber', title: '单号', rowspan: '2', width: 200, align: 'center', templet: function (d) {
+                    var str = '<a lay-event="details" class="notice-title-click">' + d.oddNumber + '</a>';
+                    if (!isNull(d.fromId)) {
+                        str += '<span class="state-new">[转]</span>';
+                    }
+                    return str;
+                }},
+            { field: 'operTime', title: '单据日期', rowspan: '2', align: 'center', width: 140 },
+            { colspan: '2', title: '来源单据信息', align: 'center' },
+            { field: 'processInstanceId', title: '流程ID', rowspan: '2', width: 100, templet: function (d) {
+                    return '<a lay-event="processDetails" class="notice-title-click">' + getNotUndefinedVal(d.processInstanceId) + '</a>';
+                }},
+            { field: 'state', title: '状态', rowspan: '2', width: 90, templet: function (d) {
+                    return skyeyeClassEnumUtil.getEnumDataNameByCodeAndKey("flowableStateEnum", 'id', d.state, 'name');
+                }},
+            { field: 'otherState', title: '出库状态', rowspan: '2', width: 90, templet: function (d) {
+                    return skyeyeClassEnumUtil.getEnumDataNameByCodeAndKey("outLetState", 'id', d.otherState, 'name');
+                }},
+            { field: 'createName', title: systemLanguage["com.skyeye.createName"][languageType], rowspan: '2', width: 120 },
+            { field: 'createTime', title: systemLanguage["com.skyeye.createTime"][languageType], rowspan: '2', align: 'center', width: 150 },
+            { field: 'lastUpdateName', title: systemLanguage["com.skyeye.lastUpdateName"][languageType], rowspan: '2', align: 'left', width: 120 },
+            { field: 'lastUpdateTime', title: systemLanguage["com.skyeye.lastUpdateTime"][languageType], rowspan: '2', align: 'center', width: 150 },
+            { title: systemLanguage["com.skyeye.operation"][languageType], rowspan: '2', fixed: 'right', align: 'center', width: 200, toolbar: '#tableBar'}
+        ], [
+            { field: 'fromTypeId', title: '来源类型', width: 150, templet: function (d) {
+                    return skyeyeClassEnumUtil.getEnumDataNameByCodeAndKey("pickFromType", 'id', d.fromTypeId, 'name');
+                }},
+            { field: 'fromId', title: '单据编号', width: 200, templet: function (d) {
+                    return getNotUndefinedVal(d.fromMation?.oddNumber);
+                }}
+        ]],
+        done: function(json) {
+            matchingLanguage();
+            initTableSearchUtil.initAdvancedSearch(this, json.searchFilter, form, "请输入单据编号", function () {
+                table.reloadData("messageTable", {page: {curr: 1}, where: getTableParams()});
+            });
+        }
+    });
+
+    function loadTable() {
+        table.reloadData("messageTable", {where: getTableParams()});
+    }
+
+    function getTableParams(){
+        return $.extend(true, {}, initTableSearchUtil.getSearchValue("messageTable"));
+    }
+
+    exports('materialReturnOrderList', {});
+});
