156da750dbef05b8e9a7c7e4c0637c2232ea5134
LAPTOP-UV1MNL38\18023 <1802330370@qq.com>
2024-07-17 11:12:26 +0800

    feat：新增车间物料库存和部门物料库存商品详情列表

--- erp-produce/src/main/resources/template/js/erpDepartStock/erpDepartStockList.js
+++ erp-produce/src/main/resources/template/js/erpDepartStock/erpDepartStockList.js
@@ -25,7 +25,9 @@ layui.config({
 			{ field: 'materialMation', title: '产品', align: 'left', width: 120, templet: function (d) {return isNull(d.materialMation) ? '' : d.materialMation.name}},
 			{ field: 'normsMation', title: '规格', align: 'left', width: 200, templet: function (d) {return isNull(d.normsMation) ? '' : d.normsMation.name}},
 			{ field: 'departmentMation', title: '处理部门', align: 'left', width: 200, templet: function (d) {return isNull(d.departmentMation) ? '' : d.departmentMation.name}},
-			{ field: 'stock', title: '部门库存', align: 'center', width: 80 }
+			{ field: 'stock', title: '部门库存', align: 'center', width: 80,templet: function (d) {
+					var str = '<a lay-event="inventory" class="notice-title-click">' + d.stock + '</a>';
+					return str;}},
 		]],
 		done: function(json) {
 			matchingLanguage();
@@ -34,6 +36,28 @@ layui.config({
 			});
 		}
 	});
+
+	table.on('tool(messageTable)', function (obj) {
+		var data = obj.data;
+		var layEvent = obj.event;
+		if (layEvent === 'inventory') { //详情
+			inventory(data);
+		}
+	});
+
+	// 部门商品库存详情
+	function inventory(data) {
+		_openNewWindows({
+			url: "../../tpl/erpDepartStock/departmentalInventoryDetails.html?materialId=" + data.materialId
+				+ "&normsId="+data.normsId + "&departmentId=" + data.departmentId,
+			title: "部门商品库存详情",
+			pageId: "productInventoryDetails",
+			area: ['90vw', '90vh'],
+			callBack: function (refreshCode) {
+				winui.window.msg(systemLanguage["com.skyeye.successfulOperation"][languageType], {icon: 1, time: 2000});
+				loadTable();
+			}});
+	}
 
 	form.render();
     $("body").on("click", "#reloadTable", function() {
