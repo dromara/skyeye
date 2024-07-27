0e3be5698c74ec2e3b989a720311360627ed757e
huangcaiyun. <14141603+huangcaiyunyun@user.noreply.gitee.com>
2024-07-27 21:08:54 +0800

    update：工序验收

--- erp-produce/src/main/resources/template/js/processAcceptance/machiningWarehouseCheck.js
+++ erp-produce/src/main/resources/template/js/processAcceptance/machiningWarehouseCheck.js
@@ -14,37 +14,50 @@ layui.config({
     var index = parent.layer.getFrameIndex(window.name);
     var $ = layui.$;
     var id = GetUrlParam("id");
+    let initFirst = false
 
-    // 工序验收
-            // 工序验收的【编辑布局】
-            dsFormUtil.initEditPageForStatic('content', 'FP2024071500001', {}, {
-                savePreParams: function (params) {
-                },
-                saveData: function (params) {
-                    // 保存数据
-                    AjaxPostUtil.request({url: sysMainMation.erpBasePath + "writeMachinProcedureAccept", params: params, type: 'json', method: "POST", callback: function(json) {
-                            parent.layer.close(index);
-                            parent.refreshCode = '0';
-                        }});
-                },
-                // loadComponentCallback: function () {
-                //     $("div[controlType='supplier']").remove();
-                //     $("div[controlType='purchaseDeliveryFromType']").remove();
-                // },
-                // tableAddRowCallback: function (tableId) {
-                //     // $("#addRow" + tableId).remove();
-                //     $("div[controlType='simpleTable']").find(".unitPrice").prop('disabled', true);
-                //     $("div[controlType='simpleTable']").find(".amountOfMoney").prop('disabled', true);
-                //     $("div[controlType='simpleTable']").find(".taxRate").prop('disabled', true);
-                //     $("div[controlType='simpleTable']").find(".taxMoney").prop('disabled', true);
-                //     $("div[controlType='simpleTable']").find(".taxUnitPrice").prop('disabled', true);
-                //     $("div[controlType='simpleTable']").find(".taxLastMoney").prop('disabled', true);
-                //     $("div[controlType='simpleTable']").find(".chooseProductBtn").prop('disabled', true);
-                //     $("div[controlType='simpleTable']").find(".normsId").prop('disabled', true);
-                //     $("div[controlType='simpleTable']").find(".qualityInspection").prop('disabled', true);
-                //     $("div[controlType='simpleTable']").find(".taxLastinspectionRatio").prop('disabled', true);
-                //
-                // }
-            });
+
+
+    // 工序验收的【编辑布局】
+    dsFormUtil.initEditPageForStatic('content', 'FP2024071500001', {}, {
+        savePreParams: function (params) {
+            params.machinProcedureFarmId=id
+        },
+        saveData: function (params) {
+            // 保存数据
+            AjaxPostUtil.request({url: sysMainMation.erpBasePath + "writeMachinProcedureAccept", params: params, type: 'json', method: "POST", callback: function(json) {
+                    parent.layer.close(index);
+                    parent.refreshCode = '0';
+                }});
+        },
+        // loadComponentCallback: function () {
+        //     $("div[controlType='supplier']").remove();
+        //     $("div[controlType='purchaseDeliveryFromType']").remove();
+        // },
+        // tableAddRowCallback: function (tableId) {
+        //     // $("#addRow" + tableId).remove();
+        //     $("div[controlType='simpleTable']").find(".unitPrice").prop('disabled', true);
+        //     $("div[controlType='simpleTable']").find(".amountOfMoney").prop('disabled', true);
+        //     $("div[controlType='simpleTable']").find(".taxRate").prop('disabled', true);
+        //     $("div[controlType='simpleTable']").find(".taxMoney").prop('disabled', true);
+        //     $("div[controlType='simpleTable']").find(".taxUnitPrice").prop('disabled', true);
+        //     $("div[controlType='simpleTable']").find(".taxLastMoney").prop('disabled', true);
+        //     $("div[controlType='simpleTable']").find(".chooseProductBtn").prop('disabled', true);
+        //     $("div[controlType='simpleTable']").find(".normsId").prop('disabled', true);
+        //     $("div[controlType='simpleTable']").find(".qualityInspection").prop('disabled', true);
+        //     $("div[controlType='simpleTable']").find(".taxLastinspectionRatio").prop('disabled', true);
+        //
+        // }
+
+        tableDeleteRowCallback: function (tableId) {
+            if (!initFirst) {
+                if (!isNull(tableId)) {
+                    initFirst = true;
+                    $("#addRow" + tableId).click();
+                }
+                // initFirst = false;
+            }
+        }
+    });
 
 });
