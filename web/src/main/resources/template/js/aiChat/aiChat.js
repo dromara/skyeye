0a356167b80e98c75564fc50a0d93966a43d664a
huangcaiyun. <14141603+huangcaiyunyun@user.noreply.gitee.com>
2024-10-15 17:25:34 +0800

    update：aichat的目录

--- web/src/main/resources/template/js/aiChat/aiChat.js
+++ web/src/main/resources/template/js/aiChat/aiChat.js
@@ -12,6 +12,7 @@ layui.config({
     let listIndex = 0;
     let page = 1;
     let loadData = true;
+    let apiKeyId = '555438cb1e3b4e328759680fdfa8d92';//默认接口
 
     systemCommonUtil.getSysCurrentLoginUserMation(function (data) {
         userMation = data.bean;
@@ -19,38 +20,44 @@ layui.config({
     });
 
     // 历史记录
-    function loadChatHistory () {
+    function loadChatHistory() {
         let params = {
             page: page,
             limit: 15,
             holderId: 'cf32034645ca4d7c9ef01e7bba81e3e6'
         }
-        AjaxPostUtil.request({url: sysMainMation.aiBasePath + "queryPageMessageList", params: params, type: 'json', method: 'POST', callback: function (json) {
-            // 将数据转为正序排序
-            let rows = resetList(json.rows);
-            rows.sort(function(a, b) {
-                return b.orderBy - a.orderBy; // 升序排序
-            });
-            $('.chat-box').prepend(getDataUseHandlebars(listMessageTemplate, rows));
-            $.each(rows, function (ii, item) {
-                if (!isNull(item.aiId)) {
-                    editormd.markdownToHTML(item.aiId, {markdown: item.content});
+        AjaxPostUtil.request({
+            url: sysMainMation.aiBasePath + "queryPageMessageList",
+            params: params,
+            type: 'json',
+            method: 'POST',
+            callback: function (json) {
+                // 将数据转为正序排序
+                let rows = resetList(json.rows);
+                rows.sort(function (a, b) {
+                    return b.orderBy - a.orderBy; // 升序排序
+                });
+                $('.chat-box').prepend(getDataUseHandlebars(listMessageTemplate, rows));
+                $.each(rows, function (ii, item) {
+                    if (!isNull(item.aiId)) {
+                        editormd.markdownToHTML(item.aiId, {markdown: item.content});
+                    }
+                });
+                if (page * 15 > json.total && loadData) {
+                    loadData = false;
+                    $('.chat-box').prepend("<div class='more'>没有更多数据..</div>");
                 }
-            });
-            if (page * 15 > json.total && loadData) {
-                loadData = false;
-                $('.chat-box').prepend("没有更多数据..").css();
+                if (page == 1) {
+                    // 滚动到底部
+                    $('.chat-box').scrollTop($('.chat-box').prop('scrollHeight'));
+                }
             }
-            if (page == 1) {
-                // 滚动到底部
-                $('.chat-box').scrollTop($('.chat-box').prop('scrollHeight'));
-            }
-        }});
+        });
     }
 
     // 向上滚动加载
-    $(document).ready(function() {
-        $('#chat-box').scroll(function(e) {
+    $(document).ready(function () {
+        $('#chat-box').scroll(function (e) {
             var elem = e.target;
             var scrollTop = elem.scrollTop;
             var scrollHeight = elem.scrollHeight;
@@ -68,6 +75,7 @@ layui.config({
         });
     });
 
+    // 遍历历史聊天记录
     function resetList(list) {
         let newList = [];
         $.each(list, function (ii, item) {
@@ -88,13 +96,14 @@ layui.config({
         return newList;
     }
 
+    // 点击发送
     $("body").on("click", "#sendMessage", function (e) {
         var messageInput = $("#messageInput").val().trim();
         if (!isNull(messageInput)) {
             let params = {
                 message: messageInput,
                 avatar: fileBasePath + userMation.userPhoto,
-                apiKeyId: 'cf32034645ca4d7c9ef01e7bba81e3e6'
+                apiKeyId: apiKeyId,
             }
             var message = getDataUseHandlebars(beanMessageTemplate, params);
             $('.chat-box').append(message);
@@ -103,28 +112,55 @@ layui.config({
             // 滚动到底部
             $('.chat-box').scrollTop($('.chat-box').prop('scrollHeight'));
             // 发送请求
-            AjaxPostUtil.request({url: "http://192.168.3.8:8120/" + "sendChatMessage", params: params, type: 'json', method: 'POST', callback: function (json) {
-                listIndex++;
-                // 添加回答
-                let bean = {
-                    aiId: "aiContentId" + listIndex,
-                    content: json.bean.content,
-                    avatar: fileBasePath + userMation.userPhoto,
+            AjaxPostUtil.request({
+                url: "http://192.168.3.8:8120/" + "sendChatMessage",
+                params: params,
+                type: 'json',
+                method: 'POST',
+                callback: function (json) {
+                    listIndex++;
+                    // 添加回答
+                    let bean = {
+                        aiId: "aiContentId" + listIndex,
+                        content: json.bean.content,
+                        avatar: fileBasePath + userMation.userPhoto,
+                    }
+                    message = getDataUseHandlebars(beanMessageTemplate, bean);
+                    $('.chat-box').append(message);
+                    editormd.markdownToHTML(bean.aiId, {markdown: bean.content});
+                    // 清空输入框
+                    $('textarea').val('');
+                    // 滚动到底部
+                    $('.chat-box').scrollTop($('.chat-box').prop('scrollHeight'));
                 }
-                message = getDataUseHandlebars(beanMessageTemplate, bean);
-                $('.chat-box').append(message);
-                editormd.markdownToHTML(bean.aiId, {markdown: bean.content});
-                // 清空输入框
-                $('textarea').val('');
-                // 滚动到底部
-                $('.chat-box').scrollTop($('.chat-box').prop('scrollHeight'));
-            }});
+            });
         }
     });
 
     $("body").on("click", ".layui-nav-item", function (e) {
+        var index = $(".layui-nav-item").index(this);
+        if (index == 0) {
+            //讯飞
+            apiKeyId = '555438cb1e3b4e328759680fdfa8d92';
+        } else if (index == 1) {
+            //文心
+            apiKeyId = '6a13b4ac47bb487c95609c8770c5955b';
+        } else if (index == 2) {
+            //通义
+            apiKeyId = '2227ff7247ee47edaa2f5a3910b907bf';
+        }
+        console.log(index,apiKeyId);
 
+        // loadApiKey(apiKeyId);
     });
+    // $(function() {
+    //     console.log(apiKeyId);
+    // });
+    // (function loop() {
+    //     console.log(apiKeyId);
+    //     setTimeout(loop, 2000);
+    // })();
+
 // 监听回车键发送消息
 // document.getElementById('messageInput').addEventListener('keypress', function (e) {
 //     if (e.key === 'Enter') {
