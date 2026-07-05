9f785ba651c683d66dbf2c8cd8fea450708f5b52
WeiZhiQiang <598748873@qq.com>
2026-07-05 17:46:15 +0800

    feat: 考勤班次-排班考勤修改

--- skyeye-adm/adm-common/src/main/java/com/skyeye/rest/erp/farm/service/IFarmService.java
+++ skyeye-adm/adm-common/src/main/java/com/skyeye/rest/erp/farm/service/IFarmService.java
@@ -2,10 +2,6 @@ package com.skyeye.rest.erp.farm.service;
 
 import com.skyeye.base.rest.service.IService;
 
-import java.util.List;
-import java.util.Map;
-
 public interface IFarmService extends IService {
 
-    List<Map<String, Object>> queryFarmByIds(String farmIds);
 }
--- skyeye-adm/adm-common/src/main/java/com/skyeye/rest/erp/farm/service/impl/IFarmServiceImpl.java
+++ skyeye-adm/adm-common/src/main/java/com/skyeye/rest/erp/farm/service/impl/IFarmServiceImpl.java
@@ -2,12 +2,15 @@ package com.skyeye.rest.erp.farm.service.impl;
 
 import com.skyeye.base.rest.service.impl.IServiceImpl;
 import com.skyeye.common.client.ExecuteFeignClient;
+import com.skyeye.common.constans.CacheConstants;
 import com.skyeye.rest.erp.farm.rest.IFarmRest;
 import com.skyeye.rest.erp.farm.service.IFarmService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 @Service
@@ -17,7 +20,17 @@ public class IFarmServiceImpl extends IServiceImpl implements IFarmService {
     private IFarmRest iFarmRest;
 
     @Override
-    public List<Map<String, Object>> queryFarmByIds(String farmIds) {
-        return ExecuteFeignClient.get(() -> iFarmRest.queryFarmByIds(farmIds)).getRows();
+    public Map<String, Object> queryEntityMationById(String id) {
+        return queryEntityMationByIds(id).stream().findFirst().orElse(new HashMap<>());
+    }
+
+    @Override
+    public List<Map<String, Object>> queryEntityMationByIds(String ids) {
+        return ExecuteFeignClient.get(() -> iFarmRest.queryFarmByIds(ids)).getRows();
+    }
+
+    @Override
+    public String queryCacheKeyById(String id) {
+        return String.format(Locale.ROOT, "%s:%s", CacheConstants.MES_FARM_CACHE_KEY, id);
     }
 }
