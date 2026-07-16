4e9f25d0e597776a27ba96494b88351b8eb63677
DESKTOP-M6VBFDN\张文艺 <3359371238@qq.com>
2026-07-16 11:57:25 +0800

    feat:修改维修备件返回为空问题

--- skyeye-seal-service/seal-service-pro/src/main/java/com/skyeye/accessory/controller/ServiceUserStockController.java
+++ skyeye-seal-service/seal-service-pro/src/main/java/com/skyeye/accessory/controller/ServiceUserStockController.java
@@ -46,13 +46,22 @@ public class ServiceUserStockController {
         serviceUserStockService.queryMyPartsNumByNormsId(inputObject, outputObject);
     }
 
-    @ApiOperation(id = "queryUserStockByNormsIds", value = "批量查询用户规格库存（服务间调用）", method = "POST", allUse = "0")
+    @ApiOperation(id = "queryUserStockByNormsIds", value = "批量查询用户规格库存（服务间调用）", method = "POST", allUse = "2")
+    @ApiImplicitParams(value = {
+        @ApiImplicitParam(id = "userId", name = "userId", value = "用户id", required = "required"),
+        @ApiImplicitParam(id = "normsIds", name = "normsIds", value = "规格id集合", required = "required")})
     @RequestMapping("/post/ServiceUserStockController/queryUserStockByNormsIds")
     public void queryUserStockByNormsIds(InputObject inputObject, OutputObject outputObject) {
         serviceUserStockService.queryUserStockByNormsIds(inputObject, outputObject);
     }
 
-    @ApiOperation(id = "editMaterialNormsUserStock", value = "增减我的配件库存（服务间调用）", method = "POST", allUse = "0")
+    @ApiOperation(id = "editMaterialNormsUserStock", value = "增减我的配件库存（服务间调用）", method = "POST", allUse = "2")
+    @ApiImplicitParams(value = {
+        @ApiImplicitParam(id = "userId", name = "userId", value = "用户id", required = "required"),
+        @ApiImplicitParam(id = "materialId", name = "materialId", value = "商品id", required = "required"),
+        @ApiImplicitParam(id = "normsId", name = "normsId", value = "规格id", required = "required"),
+        @ApiImplicitParam(id = "operNumber", name = "operNumber", value = "数量", required = "required"),
+        @ApiImplicitParam(id = "type", name = "type", value = "类型", required = "required,num")})
     @RequestMapping("/post/ServiceUserStockController/editMaterialNormsUserStock")
     public void editMaterialNormsUserStock(InputObject inputObject, OutputObject outputObject) {
         serviceUserStockService.editMaterialNormsUserStockForFeign(inputObject, outputObject);
