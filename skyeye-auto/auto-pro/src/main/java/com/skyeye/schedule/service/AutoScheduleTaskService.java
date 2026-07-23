4ecb027a155527238a0a4dcbf06bc5da3b946fdd
卢达淋 <16455818+ludalin1024@user.noreply.gitee.com>
2026-07-23 22:53:05 +0800

    feat: 执行结束返回结果修改为百分比（-1为执行中）。增加执行用例的线程池

--- skyeye-auto/auto-pro/src/main/java/com/skyeye/schedule/service/AutoScheduleTaskService.java
+++ skyeye-auto/auto-pro/src/main/java/com/skyeye/schedule/service/AutoScheduleTaskService.java
@@ -10,12 +10,17 @@ import com.skyeye.common.object.OutputObject;
 import com.skyeye.schedule.entity.AutoScheduleTask;
 
 /**
+ * @ClassName: AutoScheduleTaskService
  * @Description: 自动化定时任务服务
+ * @author: skyeye云系列--卫志强
+ * @date: 2026/7/22
+ * @Copyright: 2026 https://gitee.com/doc_wei01/skyeye Inc. All rights reserved.
+ * 注意：本内容仅限购买后使用.禁止私自外泄以及用于其他的商业目的
  */
 public interface AutoScheduleTaskService extends SkyeyeBusinessService<AutoScheduleTask> {
 
     /**
-     * 执行定时任务（解析用例范围并逐个投递执行）
+     * 执行定时任务
      */
     void executeScheduleTask(InputObject inputObject, OutputObject outputObject);
 
