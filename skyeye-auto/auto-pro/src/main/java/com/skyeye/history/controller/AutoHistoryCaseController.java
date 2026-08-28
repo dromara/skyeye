da101babf65786501aa15bdd1df2df763a22b547
WeiZhiQiang <598748873@qq.com>
2026-08-28 11:16:14 +0800

    feat: 新增用例执行历史的AI分析与自动提bug

--- skyeye-auto/auto-pro/src/main/java/com/skyeye/history/controller/AutoHistoryCaseController.java
+++ skyeye-auto/auto-pro/src/main/java/com/skyeye/history/controller/AutoHistoryCaseController.java
@@ -8,13 +8,17 @@ import com.skyeye.annotation.api.Api;
 import com.skyeye.annotation.api.ApiImplicitParam;
 import com.skyeye.annotation.api.ApiImplicitParams;
 import com.skyeye.annotation.api.ApiOperation;
+import com.skyeye.common.constans.CommonNumConstants;
 import com.skyeye.common.entity.search.CommonPageInfo;
 import com.skyeye.common.object.InputObject;
 import com.skyeye.common.object.OutputObject;
 import com.skyeye.history.service.AutoHistoryCaseService;
+import com.skyeye.history.service.impl.AutoCaseAiHistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * @ClassName: AutoHistoryCaseController
@@ -30,6 +34,9 @@ public class AutoHistoryCaseController {
 
     @Autowired
     private AutoHistoryCaseService autoHistoryCaseService;
+
+    @Autowired
+    private AutoCaseAiHistoryService autoCaseAiHistoryService;
 
     @ApiOperation(id = "queryAutoCaseHistoryList", value = "获取用例执行历史列表", method = "POST", allUse = "2")
     @ApiImplicitParams(classBean = CommonPageInfo.class)
@@ -52,6 +59,26 @@ public class AutoHistoryCaseController {
     @RequestMapping("/post/AutoHistoryCaseController/finishAutoCaseHistoryById")
     public void finishAutoCaseHistoryById(InputObject inputObject, OutputObject outputObject) {
         autoHistoryCaseService.finishAutoCaseHistoryById(inputObject, outputObject);
+    }
+
+    @ApiOperation(id = "aiGenerateHistoryAnalysis", value = "AI分析用例执行历史", method = "POST", allUse = "2")
+    @ApiImplicitParams({
+        @ApiImplicitParam(id = "historyId", name = "historyId", value = "执行历史id", required = "required")})
+    @RequestMapping("/post/AutoHistoryCaseController/aiGenerateHistoryAnalysis")
+    public void aiGenerateHistoryAnalysis(InputObject inputObject, OutputObject outputObject) {
+        Map<String, Object> bean = autoCaseAiHistoryService.generate(inputObject.getParams());
+        outputObject.setBean(bean);
+        outputObject.settotal(CommonNumConstants.NUM_ONE);
+    }
+
+    @ApiOperation(id = "aiParseHistoryAnalysis", value = "解析AI用例执行历史分析", method = "POST", allUse = "2")
+    @ApiImplicitParams({
+        @ApiImplicitParam(id = "answer", name = "answer", value = "AI完整返回文本", required = "required")})
+    @RequestMapping("/post/AutoHistoryCaseController/aiParseHistoryAnalysis")
+    public void aiParseHistoryAnalysis(InputObject inputObject, OutputObject outputObject) {
+        Map<String, Object> bean = autoCaseAiHistoryService.parseAnswer(inputObject.getParams());
+        outputObject.setBean(bean);
+        outputObject.settotal(CommonNumConstants.NUM_ONE);
     }
 
 }
